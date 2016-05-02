4de10ac72fc271e24beffa290ab4a27c28afa44f
Or David <ordavidil@gmail.com>
2016-05-02 20:34:37 +0300

    Register all models to the admin.

--- server/pension/admin.py
+++ server/pension/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Quarter, ManagingBody, Fund, Holding, Instrument
 
-# Register your models here.
+admin.site.register(Quarter)
+admin.site.register(ManagingBody)
+admin.site.register(Fund)
+admin.site.register(Instrument)
+admin.site.register(Holding)
