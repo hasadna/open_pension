3b1e4ebe40e49f30d924843a8ca60f650820f126
Nir Galon <nirgn975@gmail.com>
2016-05-07 22:00:13 +0300

    Cleanup old client and server apps.

--- client/app/main.ts
+++
@@ -1,4 +0,0 @@
-import {bootstrap}  from 'angular2/platform/browser';
-import {HelloWorld} from './hello_world';
-
-bootstrap(HelloWorld);
--- server/config/settings.py
+++ server/config/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
