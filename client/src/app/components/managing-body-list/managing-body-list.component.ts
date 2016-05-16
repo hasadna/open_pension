702236af63116bb7fe812eb483f623a08f329023
amanecer2 <sh2le86@gmail.com>
2016-05-16 20:41:02 +0300

    Build the html, css, ts. Component ready with @input,@output and emit to component father

--- client/src/app/components/managing-body-list/managing-body-list.component.ts
+++ client/src/app/components/managing-body-list/managing-body-list.component.ts
@@ -1,3 +1,34 @@
 /**
  * Created by amanecer on 16/05/2016.
  */
+import {Component,Input,Output,EventEmitter} from '@angular/core';
+import {ManagingBodyComponent} from './managing-body/managing-body.component';
+
+
+@Component({
+    selector: 'op-managing-body-list',
+    templateUrl: 'app/components/managing-body-list/managing-body-list.component.html',
+    styleUrls: ['app/components/managing-body-list/managing-body-list.component.css'],
+    providers: [],
+    directives: [ManagingBodyComponent],
+    pipes: []
+})
+export class ManagingBodyListComponent {
+
+    constructor() {
+
+    }
+    @Input() mangingBodyList : any;
+
+    @Output() notify : EventEmitter<any> = new EventEmitter<any>;
+
+
+    onNotify(name:string):void{
+        this.notify.emit(name);
+    }
+
+
+}
+
+
+
