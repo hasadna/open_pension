156825590296600ff22bb898694491f384952f69
Or David <ordavidil@gmail.com>
2016-05-17 08:09:14 +0300

    Cleanup #2

--- server/config/settings.py
+++ server/config/settings.py
@@ -99,8 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
     ],
