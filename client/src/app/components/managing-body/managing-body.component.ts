a17a9c1823a2f0bd5779091afdadbe2c333f7ca1
tarieli1 <tarieli1@gmail.com>
2016-05-17 17:59:10 +0300

    Create managing-body component (#27)
    * create managing body dir
    
    * second
    
    * stlye fix
    
    * changed stylesheet to scss
    
    * removed stylesheet file

---
+++ client/src/app/components/managing-body/managing-body.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'op-managing-body',
+  templateUrl: 'app/components/managing-body/managing-body.component.html',
+  styleUrls: [],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+
+export class ManagingBodyComponent {
+  @Input() mangingBodyData: Object;
+  @Output()notify : EventEmitter<string> = new EventEmitter();
+  
+  constructor() {}
+  
+  onClick(){
+    this.notify.emit(${this.mangingBodyData.title});
+  }
+}
