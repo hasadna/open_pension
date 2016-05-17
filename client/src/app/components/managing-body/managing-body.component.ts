3864c34b4c4d6eaf6633c1be0393f4a1540fb012
TamirArieli <tarieli1@gmail.com>
2016-05-17 17:52:30 +0300

    removed stylesheet file

--- client/src/app/components/managing-body/managing-body.component.ts
+++ client/src/app/components/managing-body/managing-body.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'op-managing-body',
   templateUrl: 'app/components/managing-body/managing-body.component.html',
-  styleUrls: ['app/components/managing-body/managing-body.component.scss'],
+  styleUrls: [],
   providers: [],
   directives: [],
   pipes: []
@@ -11,7 +11,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 export class ManagingBodyComponent {
   @Input() mangingBodyData: Object;
-  @Output() notify : EventEmitter<string> = new EventEmitter();
+  @Output()notify : EventEmitter<string> = new EventEmitter();
   
   constructor() {}
   
