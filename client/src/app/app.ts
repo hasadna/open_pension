6cbf926a47d03ea6199b49fb109c204dadcddaca
Nir Galon <nirgn975@gmail.com>
2016-05-20 00:35:23 +0300

    Add simple detail view.

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,6 +3,7 @@ import { RouteConfig, ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 
 import { Home } from './home/home.component';
 import { About } from './about/about.component';
+import { ManagingBodyDetailComponent } from './managing-body-detail/managing-body-detail.component';
 
 @Component({
   selector: 'app',
@@ -15,6 +16,7 @@ import { About } from './about/about.component';
 @RouteConfig([
   { path: '/home',       component: Home,        name: 'Home', useAsDefault: true },
   { path: '/about',      component: About,       name: 'About' },
+  { path: '/managingbody/:id',   component: ManagingBodyDetailComponent,  name: 'ManagingBodyDetailComponent' },
 ])
 
 export class App {
---
+++ client/src/app/managing-body-detail/managing-body-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'op-managing-body-detail',
+  templateUrl: 'app/managing-body-detail/managing-body-detail.component.html',
+  styleUrls: [],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+
+export class ManagingBodyDetailComponent {
+
+  constructor() {}
+
+}
