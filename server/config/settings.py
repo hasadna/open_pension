14f079ab931ea305f6bc111dca311c803474bb89
Nir Galon <nirgn975@gmail.com>
2016-05-22 08:53:38 +0300

    Re-roll from master.

--- server/config/settings.py
+++ server/config/settings.py
@@ -123,7 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Importing local settings if exists.
 try:
