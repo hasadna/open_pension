5a39408055ed24dccc09e0da00269b09354d0c41
Ran Moshe <rani.moshe@gmail.com>
2016-05-23 19:14:35 +0300

    changing class name to Api, moving file to ./api/ dir

--- server/pension/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-
-class TestPension(TestCase):
-
-    def test_index_view(self):
-        response = self.client.get('/api/')
-        self.assertEqual(response.status_code, 200)
