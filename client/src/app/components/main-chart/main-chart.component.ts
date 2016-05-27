2df8462af67c6e0d04223ba2067357e0a20e3ccd
MaaikeB <maaike.brugghen@fundbox.com>
2016-05-27 14:47:29 +0300

    main-chart component

---
+++ client/src/app/components/main-chart/main-chart.component.ts
@@ -0,0 +1,14 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'main-chart',
+  templateUrl: 'app/components/main-chart/main-chart.component.html',
+  styleUrls: [],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+
+export class MainChartComponent {
+    constructor() {}
+}
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 
 import { ManagingBodyListComponent } from '../components/managing-body-list/managing-body-list.component';
+import { MainChartComponent } from '../components/main-chart/main-chart.component';
 
 @Component({
   selector: 'op-home',
   templateUrl: 'app/home/home.component.html',
   styleUrls: [],
   providers: [],
-  directives: [ManagingBodyListComponent],
+  directives: [ManagingBodyListComponent, MainChartComponent],
   pipes: []
 })
 
