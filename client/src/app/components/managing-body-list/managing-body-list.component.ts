e8bdaad293e0a98a9c303e5d81d3c77ff997af1b
Eyal Cohen <eyalcohen4.ec@gmail.com>
2016-05-28 00:52:46 +0300

    Fixed Path On Import To MangingBodyService And Component

--- client/src/app/components/managing-body-list/managing-body-list.component.ts
+++ client/src/app/components/managing-body-list/managing-body-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Output } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 
-import { ManagingBodyComponent } from '../managing-body/managing-body.component';
-import { ManagingBody, ManagingBodyService } from '../managing-body/managing-body.service';
+import { ManagingBodyComponent } from '../../managing-body/managing-body.component';
+import { ManagingBody, ManagingBodyService } from '../../managing-body/managing-body.service';
 
 @Component({
   selector: 'op-managing-body-list',
