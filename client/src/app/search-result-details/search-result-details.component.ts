2bd1f890a37f67bf47a55e92b441d2f84a7a42cb
shahar <sh2le86@gmail.com>
2016-05-27 15:37:11 +0300

    input for lable and result

---
+++ client/src/app/search-result-details/search-result-details.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'op-search-result-detail',
+  templateUrl: 'app/search-result-detail/search-result-detail.component.html',
+  styleUrls: [],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+
+export class SearchResultDetailComponent{
+
+  constructor() {}
+}
