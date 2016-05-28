1ac3028fc14f2e6f48b57fee88cc896c6f9e87b1
nirgn975 <nirgn975@gmail.com>
2016-05-28 16:03:46 +0300

    Re-roll

---
+++ client/src/app/components/managing-body-list/managing-body-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Output } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+
+import { ManagingBodyComponent } from '../managing-body/managing-body.component';
+import { ManagingBody, ManagingBodyService } from '../managing-body/managing-body.service';
+
+@Component({
+  selector: 'op-managing-body-list',
+  templateUrl: 'app/managing-body-list/managing-body-list.component.html',
+  styleUrls: [],
+  providers: [ManagingBodyService],
+  directives: [ManagingBodyComponent],
+  pipes: []
+})
+
+export class ManagingBodyListComponent implements OnInit {
+  managingBodyList: Observable<ManagingBody[]>;
+  errorMessage: String;
+
+  constructor(
+    private managingBodyService: ManagingBodyService
+  ) {}
+
+  ngOnInit() {
+    this.getManagingBodies();
+  }
+
+  getManagingBodies() {
+    this.managingBodyList = this.managingBodyService.getManagingBodies();
+  }
+}
