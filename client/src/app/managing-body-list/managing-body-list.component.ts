7fd773cd5ef15b754d8335e236d286c09c237a45
nirgn975 <nirgn975@gmail.com>
2016-05-28 16:49:06 +0300

    Change whitespace.

--- client/src/app/managing-body-list/managing-body-list.component.ts
+++ client/src/app/managing-body-list/managing-body-list.component.ts
@@ -15,7 +15,7 @@ export class ManagingBodyListComponent implements OnInit {
   errorMessage: String;
 
   constructor(
-     private managingBodyService: ManagingBodyService
+    private managingBodyService: ManagingBodyService
   ) {}
 
   ngOnInit() {
