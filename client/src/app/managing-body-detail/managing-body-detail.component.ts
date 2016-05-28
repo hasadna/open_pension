7fa14b3692509fdc13e49c29b57da4912072c742
Eyal Cohen <eyalcohen4.ec@gmail.com>
2016-05-28 15:13:12 +0300

    Delete Managing-body-detail component selector, we dont use it in any templa te.

--- client/src/app/managing-body-detail/managing-body-detail.component.ts
+++ client/src/app/managing-body-detail/managing-body-detail.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'op-managing-body-detail',
   templateUrl: 'app/managing-body-detail/managing-body-detail.component.html',
   styleUrls: [],
   providers: [],
