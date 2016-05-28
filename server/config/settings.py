e4dfac4b9674fbde525d1619b5d2464d6967e158
Or David <ordavidil@gmail.com>
2016-05-28 19:22:03 +0300

    Fixing the import data command.

--- server/config/settings.py
+++ server/config/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+DATA_ROOT = os.path.abspath(os.path.join(BASE_DIR, "data"))
 
 # Importing local settings if exists.
 try:
