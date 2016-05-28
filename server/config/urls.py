8f94bebbdcdc25bfc0541b60d5ff32ecc9c75174
Nir Galon <nirgn975@gmail.com>
2016-05-28 21:30:45 +0300

    Add i18n_patterns to the urls.

--- server/config/urls.py
+++ server/config/urls.py
@@ -1,7 +1,14 @@
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls import url, include
 from django.contrib import admin
 
+# URLs that shouldn't be translated.
 urlpatterns = [
+
+]
+
+# URLs that should be translated.
+urlpatterns += i18n_patterns(
     url(r'^api/', include('api.urls')),
     url(r'^admin/', admin.site.urls),
-]
+)
