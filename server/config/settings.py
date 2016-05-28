950459540ac6f50edcdd9e313cb239beef1d0595
Nir Galon <nirgn975@gmail.com>
2016-05-28 16:46:25 +0300

    Handling CORS (#40)
    * Adding django-cors-headers app for Cross-Origin Resource Sharing.
    
    * Change to white list regex.

--- server/config/settings.py
+++ server/config/settings.py
@@ -12,6 +12,9 @@ SECRET_KEY = 'yx6bafd5hfp553k6(pma1)yoky5krjnm_3&+ldea6wrx66^q%-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+CORS_ORIGIN_REGEX_WHITELIST = (
+    '^(localhost:)*',
+)
 
 ALLOWED_HOSTS = []
 
@@ -29,6 +32,7 @@ INSTALLED_APPS = [
 
     'django_extensions',
     'rest_framework',
+    'corsheaders',
 
     'pension',
 ]
@@ -42,6 +46,8 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
