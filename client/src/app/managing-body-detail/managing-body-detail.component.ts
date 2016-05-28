be923e43806d09b289233c2d3ea2ceaa71f6595a
Eyal Cohen <eyalcohen4.ec@gmail.com>
2016-05-28 15:47:55 +0300

    Revert the selector delete

--- client/src/app/managing-body-detail/managing-body-detail.component.ts
+++ client/src/app/managing-body-detail/managing-body-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'op-managing-body-detail',
   templateUrl: 'app/managing-body-detail/managing-body-detail.component.html',
   styleUrls: [],
   providers: [],
