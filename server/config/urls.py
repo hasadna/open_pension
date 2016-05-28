4989ece0413779df64613277b0c2c349ec9242f1
Nir Galon <nirgn975@gmail.com>
2016-05-28 21:31:54 +0300

    Move the api url.

--- server/config/urls.py
+++ server/config/urls.py
@@ -4,11 +4,10 @@ from django.contrib import admin
 
 # URLs that shouldn't be translated.
 urlpatterns = [
-
+    url(r'^api/', include('api.urls')),
 ]
 
 # URLs that should be translated.
 urlpatterns += i18n_patterns(
-    url(r'^api/', include('api.urls')),
     url(r'^admin/', admin.site.urls),
 )
