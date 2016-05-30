2ad743d05d866082bfe880005eb0a87ea2e1f632
Eyal Cohen <eyalcohen4.ec@gmail.com>
2016-05-31 01:26:58 +0300

    Create Spaces Between Lines

--- client/src/app/managing-body/managing-body.component.ts
+++ client/src/app/managing-body/managing-body.component.ts
@@ -1,8 +1,10 @@
 import {Component, Input} from "@angular/core";
 import {ROUTER_DIRECTIVES, RouteConfig} from "@angular/router-deprecated";
+
 import {ManagingBodyDetailComponent} from "../managing-body-detail/managing-body-detail.component";
-import {ManagingBody} from "./managing-body.service";
 import {ManagingBodyListComponent} from "../managing-body-list/managing-body-list.component";
+
+import {ManagingBody} from "./managing-body.service";
 
 @Component({
   selector: 'op-managing-body',
