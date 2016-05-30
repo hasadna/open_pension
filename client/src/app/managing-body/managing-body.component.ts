ba615ff4bf4fc0f67923bf625458edbf79da003e
Eyal Cohen <eyalcohen4.ec@gmail.com>
2016-05-31 00:54:33 +0300

    Inject ManagingBody.service To The Component

--- client/src/app/managing-body/managing-body.component.ts
+++ client/src/app/managing-body/managing-body.component.ts
@@ -1,18 +1,30 @@
 import { Component, Input } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router-deprecated';
+import { RouteConfig, ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 
 import { ManagingBodyDetailComponent } from '../managing-body-detail/managing-body-detail.component';
-import { ManagingBody } from './managing-body.service';
+import { ManagingBody, ManagingBodyService } from './managing-body.service';
 
 @Component({
   selector: 'op-managing-body',
   templateUrl: 'app/managing-body/managing-body.component.html',
   directives: [ROUTER_DIRECTIVES],
+  providers: [ManagingBodyService]
 })
+
+@RouteConfig([
+  { path: '/:id',               component: ManagingBodyComponent,                name: 'ManagingBodyComponent', }
+])
 
 export class ManagingBodyComponent {
   @Input() mangingBody: ManagingBody;
-  
-  constructor() {}
+
+  constructor(
+    private managingBodyService: ManagingBodyService
+  ) {}
+
+  // TODO: Method For Get The Id From The Server, Return componentId
+  getManagingId() {
+
+  }
 
 }
