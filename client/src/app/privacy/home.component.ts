c1774263d32deec1be2d51ceca79680fed8b841c
Eyal Cohen <eyalcohen4.ec@gmail.com>
2016-06-09 18:01:42 +0300

    Create a privacy component

---
+++ client/src/app/privacy/home.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'op-privacy',
+  templateUrl: 'app/privacy/privacy.component.html',
+  // directives: [ManagingBodyListComponent],
+})
+
+export class PrivacyComponent {
+
+  constructor() { }
+  
+}
