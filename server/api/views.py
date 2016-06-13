1c0d9aa9b21d7032776716ffca554c6919d45e01
AR85 <arielrotem85@gmail.com>
2016-06-13 21:54:52 +0300

    Adding versioning

--- server/api/views.py
+++ server/api/views.py
@@ -1,8 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, serializers
 from pension.models import ManagingBody
-from api.serializers import ManagingBodySerializer
+from api.serializers import ManagingBodySerializerVersion1
+from rest_framework.versioning import URLPathVersioning
 
+class PensionVersioning(URLPathVersioning):
+    default_version = 'v1'
+    allowed_versions = ['v1']
 
 class ManagingBodyViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ManagingBody.objects.all()
-    serializer_class = ManagingBodySerializer
+    versioning_class = PensionVersioning
+
+    def get_serializer_class(self):
+        if self.request.version:
+            raise serializers.ValidationError("Versioning is not supported yet.")
+        return ManagingBodySerializerVersion1
+
