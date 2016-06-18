42338d7815f7552e1d47e435f388b8ae20e2594c
Nir Galon <nirgn975@gmail.com>
2016-06-18 19:58:34 +0300

    Try install compose in travis take 2.

--- server/config/settings.py
+++ server/config/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DATA_ROOT = os.path.abspath(os.path.join(BASE_DIR, "data"))
 
