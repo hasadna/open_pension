2161c0c3397926157765416b8b1cf11c8f50a758
eyalcohen4 <eyalcohen4.ec@Gmail.com>
2016-06-22 16:23:53 +0300

    Generate privacy component

---
+++ client/src/app/privacy/privacy.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'op-privacy',
+  templateUrl: 'privacy.component.html',
+  styleUrls: ['privacy.component.css']
+})
+export class PrivacyComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
