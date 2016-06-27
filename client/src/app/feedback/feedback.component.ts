89215ad6be5b9ff51b03a7052ed2cf93887140c6
eyalcohen4 <eyalcohen4.ec@Gmail.com>
2016-06-27 20:38:11 +0300

    import  angualr/forms to component and add to dev-dep

--- client/src/app/feedback/feedback.component.ts
+++ client/src/app/feedback/feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 
 @Component({
   moduleId: module.id,
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 
+
 import { OpAppComponent, environment } from './app/';
 
 if (environment.production) {
