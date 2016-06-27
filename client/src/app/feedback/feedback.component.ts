d589e375de9d567e08dd07523bbffb582f8bffbb
eyalcohen4 <eyalcohen4.ec@Gmail.com>
2016-06-27 22:04:35 +0300

    Order In Folders

--- client/src/app/feedback/feedback.component.ts
+++ client/src/app/feedback/feedback.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { disableDeprecatedForms, provideForms } from '@angular/forms';
-import { NgForm }    from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, REACTIVE_FORM_DIRECTIVES } from '@angular/forms';
+
+
 @Component({
   moduleId: module.id,
   selector: 'op-feedback',
---
+++ client/src/app/feedback/feedbackQuestions/feedback.question-textbox.ts
@@ -0,0 +1,11 @@
+import { QuestionBase } from './feedback.questions';
+
+export class TextboxQuestion extends QuestionBase<string> {
+  controlType = 'textbox';
+  type: string;
+
+  constructor(options: {} = {}) {
+    super(options);
+    this.type = options['type'] || '';
+  }
+}
