43c4218abc746cfe486e578b056d2de678fc26ed
eyalcohen4 <eyalcohen4.ec@Gmail.com>
2016-06-30 17:23:30 +0300

    add template to feedback-form

--- client/src/app/feedback/feedback.component.ts
+++ client/src/app/feedback/feedback.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup, REACTIVE_FORM_DIRECTIVES } from '@angular/forms';
+import { NgForm } from '@angular/common';
 
 
 @Component({
