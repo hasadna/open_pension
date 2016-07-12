06d470e40c1c557d01caf82c18e80f363d562147
Eyal Cohen <eyalcohen4.ec@Gmail.com>
2016-07-12 14:22:45 +0300

    Delete privacy.component.css file

--- client/src/app/privacy/privacy.component.ts
+++ client/src/app/privacy/privacy.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'op-privacy',
   templateUrl: 'privacy.component.html',
-  styleUrls: ['privacy.component.css']
 })
 export class PrivacyComponent {
 
