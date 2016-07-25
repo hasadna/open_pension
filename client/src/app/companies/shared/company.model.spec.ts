07d0034151ce639ebcd6308a46b22ca27c0488db
Guy Tepper <guytepper@gmail.com>
2016-07-25 19:32:58 +0300

    Fixed company model naming

--- client/src/app/companies/shared/company.model.spec.ts
+++ client/src/app/companies/shared/company.model.spec.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-unused-variable */
 
 import { addProviders, async, inject } from '@angular/core/testing';
-import {Company} from './company';
+import {Company} from './company.model';
 
 describe('Company', () => {
   it('should create an instance', () => {
