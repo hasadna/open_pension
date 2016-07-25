602e3cf56b907f4c56d2844a13031039791f0e34
RoySegall <roy.segall@gizra.com>
2016-07-25 23:12:08 +0300

    Fix CR.

--- server/config/settings.py
+++ server/config/settings.py
@@ -94,7 +94,8 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+                'UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.'
