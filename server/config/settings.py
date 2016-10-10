f9182c7425085533e1e4d532799cd4e7fbeba298
Nir Galon <nirgn975@gmail.com>
2016-10-10 18:50:36 +0300

    PEP 8 fix.

--- server/config/settings.py
+++ server/config/settings.py
@@ -155,7 +155,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-## Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
