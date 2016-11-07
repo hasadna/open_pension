81a224bd7763d52a5f3216cc06acfa7a8425cd4c
ranwahle <ran.wahle@gmail.com>
2016-11-07 23:28:42 +0200

    Delete translation from branch

--- client/src/app/middlewares/app.middlewares.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Created by ranwahle on 19/09/2016.
- */
-export const APP_Middlewares = [];
--- client/src/app/translation/lang-ar.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Created by ranwahle on 12/10/2016.
- */
-export const LANG_AR_NAME = 'ar';
