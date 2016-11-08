55425e83458e2f98092c2b613f618044091d2823
Nir Galon <nirgn975@gmail.com>
2016-11-08 23:13:31 +0200

    Remove material2, add bootstrap, style the header

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -45,7 +44,6 @@ import {
     LayoutModule,
     StoreModule.provideStore(reducer),
     StoreDevtoolsModule.instrumentOnlyWithExtension(),
-    MaterialModule.forRoot(),
     // EffectsModule.run(ExampleEffects),
   ],
   providers: [],
