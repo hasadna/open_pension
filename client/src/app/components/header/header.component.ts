2a9db27ef1a4b4167446e2e50c86b5727fd6a4de
Ran Wahle <ran.wahle@gmail.com>
2016-11-10 21:12:32 +0200

    Header section (#186)
    * Upgrade ng-cli to beta 14
    
    * #154 add redux
    
    * #158  header section
    
    * #158  header section - icons
    Redux and translation
    
    * #158 Add logo for open pension
    
    * #158 CSS changes
    
    * #158 Correct e2e test page OpPage instead of AngularCliTestPage
    
    * #158 Use customBrowsers in karma.conf
    
    * #158 Correct js compilation error on karm.conf.js
    
    * #158 use 'Chorme without sandbox' as browser
    
    * #158 use 'Chorme without sandbox' as browser
    
    * #158 add nohup bash  script
    
    * #158 undo add nohup bash  script
    
    * #158 add sleep script afte xvfb start
    
    * #158 add sleep script afte xvfb start
    
    * #158 undo  add sleep script afte xvfb start
    
    * Delete translation from branch
    
    * new component structure
    
    * new component structure
    
    * Add new logo and cleanup the PR
    
    * Remove fonts, and revert changes
    
    * Add global scss vars
    
    * Add material2 and change icons to use it
    
    * Basic style for the header
    
    * Try temp workaround for grid layout
    
    * Remove material2, add bootstrap, style the header
    
    * Cleanup
    
    * Replace logo to svg file
    
    * Center verticaly the logo

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'op-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class HeaderComponent {
 }
