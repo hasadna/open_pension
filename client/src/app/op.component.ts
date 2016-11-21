aecf61332d0ad141731627e23ab4b8512e66ccf6
Omer Schechter <omers.iphone@gmail.com>
2016-11-21 21:35:45 +0200

    Positioning the footer at the bottom with position: absolute. Removing 'row' html element. Removing the ngOnInit lifecycle hook.

--- client/src/app/op.component.ts
+++ client/src/app/op.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'op-root',
-  template: `
-    <op-header></op-header>
-    <router-outlet></router-outlet>
-    <op-footer></op-footer>
-  `,
-  styles: [``]
+	selector: 'op-root',
+	template: `
+		<op-header></op-header>
+		<router-outlet></router-outlet>
+		<op-footer></op-footer>
+	`,
+	styles: [``]
 })
 export class OpComponent {
 }
