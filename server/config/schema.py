c2d877d45df7c96300ca179e0dc007d6107b4e74
Nir Galon <nirgn975@gmail.com>
2016-11-28 19:16:04 +0200

    Cleanup

--- server/config/schema.py
+++ server/config/schema.py
@@ -7,5 +7,4 @@ from graphene_django.debug import DjangoDebug
 class Query(pension.schema.Query, graphene.ObjectType):
     debug = graphene.Field(DjangoDebug, name='__debug')
 
-
 schema = graphene.Schema(query=Query)
