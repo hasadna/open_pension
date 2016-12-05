b23f9ea5f2c099a64217df9c34a826a58df99503
ranwahle <ranwahle@Rans-MacBook-Pro.local>
2016-12-05 21:42:08 +0200

    Remove redundent import Correct spacing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { PaiComponent } from './components';
-import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { PaiComponent, ContactUsComponent } from './components';
 
 const routes: Routes = [
   // { path: '', redirectTo: '/', pathMatch: 'full' }
   { path: '', component: PaiComponent },
-  {path: 'contact-us', component:  ContactUsComponent}
+  { path: 'contact-us', component: ContactUsComponent }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
