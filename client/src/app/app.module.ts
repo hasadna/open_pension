02b6ec018045255b296286f75cd2f6e0b4759cfd
ranwahle <ranwahle@Rans-MacBook-Pro.local>
2016-12-05 22:20:50 +0200

    Remove redundent import

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
-import { AppRoutingModule } from './app-routing.module';
-import { OpComponent } from './op.component';
+import {AppRoutingModule} from './app-routing.module';
+import {OpComponent} from './op.component';
 import reducer from './reducers';
 
 import {
@@ -48,4 +48,5 @@ import {
   providers: [],
   bootstrap: [OpComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
