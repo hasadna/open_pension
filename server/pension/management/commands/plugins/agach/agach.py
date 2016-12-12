b3459c7cc5ad08359c6993255c99929787e84ecf
RoySegall <roy.segall@gizra.com>
2016-12-12 19:44:01 +0200

    First sketch of the plugin

--- server/pension/management/commands/plugins/agach/agach.py
+++ server/pension/management/commands/plugins/agach/agach.py
@@ -12,7 +12,7 @@ class PluginOne(IPlugin):
     def parseBody(self, command, value, contexts):
         row_context = command.is_context(value, contexts)
         if row_context:
-            # Get the current context
+            # Get the current context.
             self.local_context = command.english_text(row_context)
 
             # Check if the current context is a global context or.
