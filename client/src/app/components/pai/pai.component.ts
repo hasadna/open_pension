68957bf3d7008d05db7923e8bb0475c66faac6c2
Barak Drechsler <barak.drechsler@teamaol.com>
2016-12-16 11:05:00 +0200

    (WIP) adding d3 v4 modules

--- client/src/app/components/pai/pai.component.ts
+++ client/src/app/components/pai/pai.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as scale from 'd3-scale';
+import * as select from 'd3-selection';
+import * as hierarchy from 'd3-hierarchy';
 @Component({
   selector: 'op-pai',
   templateUrl: './pai.component.html',
@@ -7,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaiComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+        debugger;
+        const hierarchy1 = hierarchy;
+        const select1 = select;
+        const scale1 = scale;  
+   }
 
   ngOnInit() {
   }
