9055f0cf127a4138bdf1e001db11dc9de038799f
Omer Schechter <omers.iphone@gmail.com>
2016-12-17 18:02:38 +0200

    Adding the route for the PostsComponent.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PaiComponent } from './components/pai/pai.component';
 import { AboutComponent } from './components/about/about.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: '/', pathMatch: 'full' }
   { path: '', component: PaiComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'blog', component: PostsComponent },
 ];
 
 @NgModule({
