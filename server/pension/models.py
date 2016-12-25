6af8573b9817e5e9e0055a7b214710a8b04b9e55
MaaikeB <maaike.brugghen@fundbox.com>
2016-12-25 10:12:59 +0200

    changes

--- server/pension/models.py
+++ server/pension/models.py
@@ -4,28 +4,10 @@ from django.db import models
 from ckeditor.fields import RichTextField
 from django.utils.translation import ugettext_lazy as _
 
-class Security(models.Model):
-    name = models.CharField(_('name'), max_length=255, blank=True)
-    security_number = models.IntegerField('security_number')
-    asset_type = models.CharField('asset_type', max_length=255, blank=True)  # type like cash, government-bond, commercial-bond, etc
-    rating = models.CharField(_('rating'), max_length=255)                      # not for Share
-    rater = models.CharField(_('rater'), max_length=255)                        # not for Share
-    currency = models.CharField(_('currency'), max_length=255)
-    interest_rate_percentage = models.IntegerField(_('interest_rate_percentage'))
-    yield_to_maturity_percentage = models.IntegerField(_('yield_to_maturity_percentage'))
-
-    # values below not for Cash
-    activity_industry = models.IntegerField(_('activity_industry'))
-    average_life_span_years = models.IntegerField(_('average_life_span_years'))
-    exchange_rate_agorot = models.IntegerField(_('exchange_rate_agorot'))
-    fair_value = models.IntegerField(_('fair_value'))
-
-    class Meta:
-        verbose_name = _('Instrument')
-        verbose_name_plural = _('Instrument')
-
 class ManagingBody(models.Model):
     name = models.CharField(_('name'), max_length=255, blank=False, unique=True)
+
+    # amitim, clal, menora, migdal, psagot, harel, other
 
     class Meta:
         verbose_name = _('ManagingBody')
@@ -33,27 +15,76 @@ class ManagingBody(models.Model):
 
 class Quarter(models.Model):
     year = models.IntegerField(_('year'), max_length=4, blank=False)
-    year_quarter = models.IntegerField(_('year_quarter'), max_length=1, blank=False)
+    year_quarter = models.IntegerField(_('year_quarter'), blank=False)
 
     class Meta:
         verbose_name = _('Quarter')
         verbose_name_plural = _('Quarter')
 
+class AssetType():
+    # see sheet for values
+    pass
+
+class ActivityIndustry():
+    # see sheet for values
+    pass
+
+class Currency():
+    # see sheet for values
+    pass
+
+class Rating():
+    # prime, high, upper_medium, lower_medium, speculative, highly_speculative, risk
+    pass
+
+class Security(models.Model):
+    """
+    Model representing a Security, but not the holding of a Security by a ManagingBody
+    """
+    name = models.CharField(_('name'), max_length=255, null=True)
+    security_number = models.IntegerField('security_number')
+    asset_type = models.CharField('asset_type', max_length=255, null=True)
+    rating = models.CharField('rating', max_length=255, null=True)
+    rater = models.CharField('rater', max_length=255)
+    currency = models.CharField('currency', max_length=255, choices=[])
+    activity_industry = models.IntegerField('activity_industry', choices=[])
+    life_span = models.IntegerField('average_life_span_years')
+    interest_rate = models.IntegerField('interest_rate')
+    rate_of_ipo = models.IntegerField('rate_of_ipo')
+    fair_value = models.IntegerField('fair_value')
+
+    liquidity = models.BooleanField('liquidity')
+    geographical_location = models.CharField('geographical_location', max_length=255)
+    reference_type = models.CharField('reference_type', max_length=255)
+    # local_context?
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.name
+
+    class Meta:
+        verbose_name = _('Security')
+        verbose_name_plural = _('Security')
+        ordering = ["name"]
+
 class Holding(models.Model):
-    managing_body_name = models.ManyToManyField(ManagingBody, _('name'), max_length=255, blank=True)   # FK
-    quarter = models.ManyToManyField(Quarter, _('quarter'), max_length=255, blank=True)            # FK
-    security_name = models.ManyToManyField(Security, _('name'), max_length=255, blank=True)        # FK
+    managing_body = models.ManyToManyField(ManagingBody, _('name'), max_length=255, blank=True)
+    quarter = models.ManyToManyField(Quarter, _('quarter'))
+    security_name = models.ManyToManyField(Security, _('name'), max_length=255, blank=True)
 
-    par_value = models.IntegerField(_('par_value'))   # how much the company holds of the security
-    par_value_percentage = models.IntegerField(_('par_value_percentage'))    # percentage of the security amount of the total of all securities the managingbody holds
-    market_cap_thousands_nis = models.IntegerField(_('market_cap_thousands_nis'))
-    investment_assets_rate = models.IntegerField(_('investment_assets_rate'))
+    acquisition_date = models.IntegerField(_('acquisition_date'))
+    market_cap = models.IntegerField(_('market_cap'))       # thousand nis
+    par_value = models.IntegerField(_('par_value'))         # percentage of the security of total investment of fund
+    linkage_type = models.IntegerField(_('linkage_type'))   # percentage of the security held by fund out of total issues amount
+    part_of_total_investment = models.IntegerField(_('part_of_total_investment'))
 
+    exchange_rate_agorot = models.IntegerField(_('exchange_rate_agorot'))
 
     class Meta:
         verbose_name = _('Holding')
         verbose_name_plural = _('Holding')
-
 
 
 # ==========================================
