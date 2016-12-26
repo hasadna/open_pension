be24481b99bd7764a7fed260a1e5d5be16281954
ranwahle <ran.wahle@gmail.com>
2016-12-26 22:48:37 +0200

    Merge branch 'master' into 196
    # Conflicts:
    #	client/package.json
    #	client/src/app/app-routing.module.ts
    #	client/src/app/app.module.ts
    #	client/src/app/components/index.ts

--- client/src/app/components/how-it-works/how-it-works.component.ts
+++ client/src/app/components/how-it-works/how-it-works.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit} from '@angular/core';
 
 declare var introJs;
 
@@ -7,12 +7,22 @@ declare var introJs;
   templateUrl: './how-it-works.component.html',
   styleUrls: ['./how-it-works.component.scss']
 })
-export class HowItWorksComponent implements OnInit {
+export class HowItWorksComponent implements OnInit, AfterViewInit {
+  ngAfterViewInit(): void {
+    introJs().setOptions({nextLabel: 'הבא', prevLabel: 'הקודם',
+      doneLabel: 'סיום', skipLabel: 'יציאה'})
+      .start();
+  }
 
+  intros: string[] = [
+    'כדי לחזור אחורה בעומק הפאי, אפשר ללחוץ על הפרמטרים העליונים שכבר נעשתה בהם בחירה',
+    'כדי לראות את הפרמטר הבא ברשימה צריך ללחוץ על פלח במעגל הפנימי ואז הוא יהפוך להיות מרכז המעגל ופרמטר נוסף יתווסף כמעגל חיצוני מרשימת הפרמטרים '
+
+  ]
   constructor() { }
 
   ngOnInit() {
-    introJs().start();
+
   }
 
 }
