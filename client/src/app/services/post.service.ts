b74ad133badedd8ecc982ddaf19320fc8df8b069
Omer Schechter <omers.iphone@gmail.com>
2017-01-02 23:11:08 +0200

    Creating the basic getPost function in the post service.

--- client/src/app/services/post.service.ts
+++ client/src/app/services/post.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
 
+import { Post } from '../models/post';
+
 @Injectable()
 export class PostService {
 
   constructor() { }
 
+  getPost(postId: number): Post {
+
+  }
 }
