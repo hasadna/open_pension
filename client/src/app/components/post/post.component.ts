a37a3694535b964e40c32b88db7eced0818fbd9d
Omer Schechter <omers.iphone@gmail.com>
2017-01-02 23:08:20 +0200

    Creating the post service. Adding basic HTML for the post component. Importing the post service in the post component.

--- client/src/app/components/post/post.component.ts
+++ client/src/app/components/post/post.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PostService } from '../../services/post.service';
+import { Post } from '../../models/post';
+
 @Component({
   selector: 'op-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
+  providers: [PostService]
 })
 export class PostComponent implements OnInit {
+  post: Post;
 
-  constructor() { }
+  constructor(
+    private postService: PostService
+  ) { }
 
   ngOnInit() {
+    this.post = this.postService.getPost();
   }
 
 }
