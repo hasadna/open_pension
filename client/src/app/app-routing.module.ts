7b2d011d1f8fd7efd0d7798db42a9ba1f481078e
ranwahle <ran.wahle@gmail.com>
2017-01-30 23:57:41 +0200

    Restore whitespace on import statements Restore import statements and routes on routing module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {PaiComponent} from "./components/pai/pai.component";
-import {HowItWorksComponent} from "./components/how-it-works/how-it-works.component";
+import { PaiComponent} from "./components/pai/pai.component";
+import { HowItWorksComponent} from "./components/how-it-works/how-it-works.component";
+import { AboutComponent} from "./components/about/about.component";
+import { PostsComponent} from "./components/posts/posts.component";
+import { PostComponent} from "./components/post/post.component";
 
 
 const routes: Routes = [
   // { path: '', redirectTo: '/', pathMatch: 'full' }
   { path: '', component: PaiComponent },
-  {path: 'how-it-works', component: HowItWorksComponent}
+  { path: 'how-it-works', component: HowItWorksComponent},
+  { path: 'about', component: AboutComponent },
+  { path: 'blog', component: PostsComponent },
+  { path: 'blog/:id', component: PostComponent }
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,22 +8,22 @@ import {HttpModule} from '@angular/http';
 import {MaterialModule} from '@angular/material';
 import {FlexLayoutModule} from '@angular/flex-layout';
 
-import {StoreModule} from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
 import {EffectsModule} from '@ngrx/effects';
-import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import {AppRoutingModule} from './app-routing.module';
-import {OpComponent} from './op.component';
+import { AppRoutingModule } from './app-routing.module';
+import { OpComponent} from './op.component';
 import reducer from './reducers';
-import {HowItWorksComponent} from './components/how-it-works/how-it-works.component';
-import {HeaderComponent} from "./components/header/header.component";
-import {SearchComponent} from "./components/search/search.component";
-import {FooterComponent} from "./components/footer/footer.component";
-import {PaiComponent} from "./components/pai/pai.component";
-import {FiltersComponent} from "./components/filters/filters.component";
-import {AboutComponent} from "./components/about/about.component";
-import {PostComponent} from "./components/post/post.component";
-import {PostsComponent} from "./components/posts/posts.component";
+import { HowItWorksComponent } from './components/how-it-works/how-it-works.component';
+import { HeaderComponent } from "./components/header/header.component";
+import { SearchComponent } from "./components/search/search.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { PaiComponent } from "./components/pai/pai.component";
+import { FiltersComponent } from "./components/filters/filters.component";
+import { AboutComponent } from "./components/about/about.component";
+import { PostComponent } from "./components/post/post.component";
+import { PostsComponent } from "./components/posts/posts.component";
 
 @NgModule({
   declarations: [
