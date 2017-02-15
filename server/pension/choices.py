77b860d4e2fac08b17108a68fe9dfc787d9913b5
nirgn975 <nir@galon.io>
2017-02-15 17:44:11 +0200

    Finish all the 'Not Negotiable' bonds

--- server/pension/choices.py
+++ server/pension/choices.py
@@ -1,71 +1,33 @@
-
-CURRENCIES = (
-    ('nis', 'NIS'),
-    ('usd', 'USD'),
+YEARS = (
+    ('2016', '2016'),
+    ('2015', '2015'),
+    ('2014', '2014'),
+    ('2013', '2013'),
+    ('2012', '2012'),
 )
 
-ASSET_TYPES = (
-    ('d', 'government_bonds'),
-    ('o', 'commercial_bonds'),
-    ('a', 'corporate_bonds'),
-    ('r', 'shares'),
-    ('r', 'etf'),
-    ('r', 'mutual_funds'),
-    ('r', 'investment_funds'),
-    ('r', 'option_warrants'),
-    ('r', 'options'),
-    ('r', 'future_contracts'),
-    ('r', 'structured_products'),
-    ('r', 'loans'),
-    ('r', 'deposits'),
-    ('r', 'land_rights'),
-    ('r', 'other_investments'),
-    ('r', 'balances_investment_commitment'),
-)
-
-RATING = (
-    ('p', 'prime'),
-    ('h', 'high'),
-    ('um', 'upper_medium'),
-    ('lm', 'lower_medium'),
-    ('s', 'speculative'),
-    ('hs', 'highly_speculative'),
-    ('r', 'risk')
-)
-
-ACTIVITY_INDUSTRY = (
-    ('C', 'cash'),
-    ('GB', 'government_bonds'),
-    ('COB', 'commercial_bonds'),
-    ('CRB', 'corporate_bonds'),
-    ('SH', 'shares'),
-    ('ETF', 'etf'),
-    ('MF', 'mutual_funds'),
-    ('IF', 'investment_funds'),
-    ('OW', 'option_warrants'),
-    ('O', 'options'),
-    ('FC', 'future_contracts'),
-    ('SP', 'structured_products'),
-    ('LR', 'land_rights'),
-    ('OI', 'other_investments'),
-    ('BIC', 'balances_investment_commitment')
-)
-
-QUARTER = (
-    ('2015', '04'),
+MONTHS = (
+    ('01', '01'),
+    ('02', '02'),
+    ('03', '03'),
+    ('04', '04'),
 )
 
 MANAGING_BODIES = (
-    ('AMT', 'amitim'),
-    ('CLL', 'clal'),
-    ('MNR', 'menora'),
-    ('MGD', 'migdal'),
-    ('PSG', 'psagot'),
-    ('HRL', 'harel'),
-    ('THR', 'other'),
+    ('AS', 'Altshuler Shaham'),
+    ('YL', 'Yelin Lapidot'),
+    ('CLL', 'Clal Insurance'),
+    ('AMT', 'Amitim'),
+    ('MTD', 'Meitav Dash'),
+    ('FNX', 'Phoenix'),
+    ('HRL', 'Harel'),
+    ('MNR', 'Menora Mivtachim'),
+    ('MGD', 'Migdal'),
+    ('PSG', 'Psagot'),
+    ('XNS', 'Excellence'),
 )
 
 GEOGRAPHICAL_LOCATION = (
-    ('IL', 'israel'),
-    ('ABR', 'abroad')
+    ('IL', 'Israel'),
+    ('ABR', 'Abroad')
 )
--- server/pension/models.py
+++ server/pension/models.py
@@ -1,87 +1,13 @@
-import uuid
-
 from django.db import models
-from ckeditor.fields import RichTextField
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
-from pension.choices import CURRENCIES, ASSET_TYPES, RATING, ACTIVITY_INDUSTRY, QUARTER, \
-    MANAGING_BODIES, GEOGRAPHICAL_LOCATION
+from pension.choices import YEARS, MONTHS, MANAGING_BODIES, GEOGRAPHICAL_LOCATION
 
 
-class Security(models.Model):
-    """
-    Model representing a Security, regardless of time and its holding by a specific ManagingBody
-    """
-    security_name = models.CharField(_('security_name'), max_length=255)
-    security_number = models.IntegerField('security_number')
-    asset_type = models.CharField('asset_type', max_length=255, choices=ASSET_TYPES, default='d')
-    currency = models.CharField('currency', max_length=255, choices=CURRENCIES, default='d')
-    activity_industry = models.IntegerField('activity_industry', choices=ACTIVITY_INDUSTRY, default='d')
+# managing_body = models.CharField('Managing Body', max_length=255, choices=MANAGING_BODIES)
+# geographical_location = models.CharField('Geographical Location', max_length=255, choices=GEOGRAPHICAL_LOCATION)
 
-    # 'subcategories' - noted in sheets as row listings, not in columns
-    is_liquid = models.BooleanField('is_liquid', default=True)
-    geographical_location = models.CharField('geographical_location', max_length=255, null=True)
-    reference_type = models.CharField('reference_type', max_length=255, null=True)
-    local_context = models.CharField('local_context', max_length=255, null=True)
-
-    def __str__(self):
-        """
-        String for representing the Model object.
-        """
-        return self.name
-
-    class Meta:
-        verbose_name = _('Security')
-        verbose_name_plural = _('Security')
-        ordering = ["security_name"]
-
-
-# rating	rater	life_span	interest_rate	rate_of_ipo	exchange_rate
-class SecurityQuarter(models.Model):
-    """
-    Model representing the Security in a specific quarter
-    """
-    security = models.ForeignKey(Security, null=True)
-    quarter = models.CharField('quarter', max_length=255, null=True, choices=QUARTER, default='d')
-    rating = models.CharField('rating', max_length=255, null=True, choices=RATING, default='d')
-    rater = models.CharField('rater', max_length=255, null=True)
-    life_span = models.IntegerField('average_life_span_years', null=True)
-    interest_rate = models.IntegerField('interest_rate', null=True)
-    rate_of_ipo = models.IntegerField('rate_of_ipo', null=True)
-    exchange_rate_agorot = models.IntegerField('exchange_rate_agorot', null=True)
-
-    def __str__(self):
-        """
-        String for representing the Model object.
-        """
-        return self.name
-
-    class Meta:
-        verbose_name = _('SecurityQuarter')
-        verbose_name_plural = _('SecurityQuarter')
-        ordering = ["rating"]
-
-
-class Holding(models.Model):
-    managing_body = models.CharField('managing_body', max_length=255, choices=MANAGING_BODIES, null=True)
-    security_quarter = models.ForeignKey(SecurityQuarter, max_length=255, null=True)
-
-    fair_value = models.IntegerField('fair_value', null=True)
-    acquisition_date = models.IntegerField('acquisition_date', null=True)
-    market_cap = models.IntegerField('market_cap', null=True)       # thousand nis
-    # percentage of the security of total investment of fund
-    par_value = models.IntegerField('par_value', null=True)
-    # percentage of the security held by fund out of total issues amount
-    linkage_type = models.IntegerField('linkage_type', null=True)
-    part_of_total_investment = models.IntegerField('part_of_total_investment', null=True)
-
-    class Meta:
-        verbose_name = _('Holding')
-        verbose_name_plural = _('Holding')
-
-
-# ==========================================
 
 def validate_percentage(value):
     if 0 > value > 100:
@@ -89,6 +15,11 @@ def validate_percentage(value):
             _('%(value)s is not a percentage (less then 0 or more then 100)'),
             params={'value': value},
         )
+
+
+class Quarter(models.Model):
+    year = models.PositiveIntegerField(_('Year'), choices=YEARS)
+    month = models.PositiveIntegerField(_('Month'), choices=MONTHS)
 
 
 class Bonds(models.Model):
@@ -108,12 +39,12 @@ class MarketableBonds(Bonds):
                                                  validators=[validate_percentage], max_digits=50, decimal_places=3)
 
 
-class ExtendedMarketableBonds(MarketableBonds):
+class RatedMarketableBonds(models.Model):
     rate = models.CharField(_('Rate'), max_length=255)
     rating_name = models.CharField(_('Rating Name'), max_length=255)
 
 
-class ExtendedExtendedMarketableBonds(ExtendedMarketableBonds):
+class ExtendedMarketableBonds(models.Model):
     average_life_span = models.DecimalField(_('Average Life Span'), max_digits=50, decimal_places=3)
     interest_rate = models.DecimalField(_('Interest Rate'), help_text="This is a percentage value.",
                                         validators=[validate_percentage], max_digits=50, decimal_places=3)
@@ -121,134 +52,187 @@ class ExtendedExtendedMarketableBonds(ExtendedMarketableBonds):
                                             validators=[validate_percentage], max_digits=50, decimal_places=3)
 
 
-class NoneExtendedExtendedMarketableBonds(ExtendedExtendedMarketableBonds):
+class PurchasableDateBonds(models.Model):
     purchase_date = models.DateField(_('Purchase Date'))
 
 
-class CashAndDeposit(models.Model):
-    pass
+class ActivityIndustryBonds(models.Model):
+    activity_industry = models.CharField(_('Activity Industry'), max_length=255)
+
+
+class CashAndDeposit(Quarter, RatedMarketableBonds):
+    name = models.CharField(_('Name'), max_length=255)
+    id = models.PositiveIntegerField(_('Id'))
+    currency = models.CharField(_('Currency'), max_length=255)
+    interest_rate = models.DecimalField(_('Interest Rate'), help_text="This is a percentage value.",
+                                        validators=[validate_percentage], max_digits=50, decimal_places=3)
+    yield_to_maturity = models.DecimalField(_('Yield To Maturity'), help_text="This is a percentage value.",
+                                            validators=[validate_percentage], max_digits=50, decimal_places=3)
+    market_value = models.DecimalField(_('Market Value'), help_text="Value is in thousands.",
+                                       max_digits=50, decimal_places=3)
+    investment_assets_rate = models.DecimalField(_('Investment Assets Rate'), help_text="This is a percentage value.",
+                                                 validators=[validate_percentage], max_digits=50, decimal_places=3)
 
     class Meta:
         verbose_name = _('Cash And Deposit')
         verbose_name_plural = _('Cash And Deposit')
 
 
-class GovernmentBonds(ExtendedExtendedMarketableBonds):
+class GovernmentBonds(Quarter, ExtendedMarketableBonds, MarketableBonds, RatedMarketableBonds):
     class Meta:
         verbose_name = _('Government Bonds')
         verbose_name_plural = _('GovernmentBonds')
 
 
-class CommercialBonds(ExtendedExtendedMarketableBonds):
-    activity_industry = models.CharField(_('Activity Industry'), max_length=255)
-
+class CommercialBonds(Quarter, ExtendedMarketableBonds, MarketableBonds, RatedMarketableBonds, ActivityIndustryBonds):
     class Meta:
         verbose_name = _('Commercial Bonds')
         verbose_name_plural = _('Commercial Bonds')
 
 
-class CorporateBonds(ExtendedExtendedMarketableBonds):
-    activity_industry = models.CharField(_('Activity Industry'), max_length=255)
-
+class CorporateBonds(Quarter, ExtendedMarketableBonds, MarketableBonds, RatedMarketableBonds, ActivityIndustryBonds):
     class Meta:
         verbose_name = _('Corporate Bonds')
         verbose_name_plural = _('Corporate Bonds')
 
 
-class Shares(MarketableBonds):
-    activity_industry = models.CharField(_('Activity Industry'), max_length=255)
-
+class Shares(Quarter, MarketableBonds, ActivityIndustryBonds):
     class Meta:
         verbose_name = _('Shares')
         verbose_name_plural = _('Shares')
 
 
-class ETF(MarketableBonds):
+class ETF(Quarter, MarketableBonds):
     class Meta:
         verbose_name = _('ETF')
         verbose_name_plural = _('ETF')
 
 
-class TrustFunds(ExtendedMarketableBonds):
-    activity_industry = models.CharField(_('Activity Industry'), max_length=255)
-
+class TrustFunds(Quarter, MarketableBonds, RatedMarketableBonds, ActivityIndustryBonds):
     class Meta:
         verbose_name = _('Trust Funds')
         verbose_name_plural = _('Trust Funds')
 
 
-class OptionWarrants(MarketableBonds):
-    activity_industry = models.CharField(_('Activity Industry'), max_length=255)
-
+class OptionWarrants(Quarter, MarketableBonds, ActivityIndustryBonds):
     class Meta:
         verbose_name = _('Option Warrants')
         verbose_name_plural = _('Option Warrants')
 
 
-class Options(MarketableBonds):
-    activity_industry = models.CharField(_('Activity Industry'), max_length=255)
-
+class Options(Quarter, MarketableBonds, ActivityIndustryBonds):
     class Meta:
         verbose_name = _('Options')
         verbose_name_plural = _('Options')
 
 
-class Futures(Bonds):
-    activity_industry = models.CharField(_('Activity Industry'), max_length=255)
-
+class Futures(Quarter, Bonds, ActivityIndustryBonds):
     class Meta:
         verbose_name = _('Futures')
         verbose_name_plural = _('Futures')
 
 
-class StructuredProducts(ExtendedExtendedMarketableBonds):
+class StructuredProducts(Quarter, ExtendedMarketableBonds, MarketableBonds, RatedMarketableBonds, PurchasableDateBonds):
     base_asset = models.CharField(_('Base Asset'), max_length=255)
-    purchase_date = models.DateField(_('Purchase Date'))
 
     class Meta:
         verbose_name = _('Structured Products')
         verbose_name_plural = _('Structured Products')
 
 
-class NotNegotiableGovernmentBonds(NoneExtendedExtendedMarketableBonds):
+class NotNegotiableGovernmentBonds(Quarter, ExtendedMarketableBonds, MarketableBonds, RatedMarketableBonds,
+                                   PurchasableDateBonds):
     class Meta:
-        verbose_name = _('Government Bonds')
-        verbose_name_plural = _('GovernmentBonds')
+        verbose_name = _('Not Negotiable Government Bonds')
+        verbose_name_plural = _('Not Negotiable GovernmentBonds')
 
 
-class NotNegotiableCommercialBonds(NoneExtendedExtendedMarketableBonds):
-    activity_industry = models.CharField(_('Activity Industry'), max_length=255)
+class NotNegotiableCommercialBonds(Quarter, ExtendedMarketableBonds, MarketableBonds, RatedMarketableBonds,
+                                   PurchasableDateBonds, ActivityIndustryBonds):
+    class Meta:
+        verbose_name = _('Not Negotiable Commercial Bonds')
+        verbose_name_plural = _('Not Negotiable Commercial Bonds')
+
+
+class NotNegotiableCorporateBonds(Quarter, ExtendedMarketableBonds, MarketableBonds, RatedMarketableBonds,
+                                  PurchasableDateBonds, ActivityIndustryBonds):
+    class Meta:
+        verbose_name = _('Not Negotiable Corporate Bonds')
+        verbose_name_plural = _('Not Negotiable Corporate Bonds')
+
+
+class NotNegotiableShares(Quarter, MarketableBonds, ActivityIndustryBonds):
+    class Meta:
+        verbose_name = _('Not Negotiable Shares')
+        verbose_name_plural = _('Not Negotiable Shares')
+
+
+class NotNegotiableInvestmentFunds(Quarter, MarketableBonds, PurchasableDateBonds, ActivityIndustryBonds):
+    class Meta:
+        verbose_name = _('Not Negotiable Investment Funds')
+        verbose_name_plural = _('Not Negotiable Investment Funds')
+
+
+class NotNegotiableOptionWarrants(Quarter, MarketableBonds, PurchasableDateBonds, ActivityIndustryBonds):
+    class Meta:
+        verbose_name = _('Not Negotiable Option Warrants')
+        verbose_name_plural = _('Not Negotiable Option Warrants')
+
+
+class NotNegotiableOptions(Quarter, MarketableBonds, PurchasableDateBonds, ActivityIndustryBonds):
+    class Meta:
+        verbose_name = _('Not Negotiable Options')
+        verbose_name_plural = _('Not Negotiable Options')
+
+
+class NotNegotiableFutures(Quarter, Bonds, PurchasableDateBonds, ActivityIndustryBonds):
+    market_value = models.DecimalField(_('Market Value'), help_text="Value is in thousands.",
+                                       max_digits=50, decimal_places=3)
+    investment_assets_rate = models.DecimalField(_('Investment Assets Rate'), help_text="This is a percentage value.",
+                                                 validators=[validate_percentage], max_digits=50, decimal_places=3)
 
     class Meta:
-        verbose_name = _('Commercial Bonds')
-        verbose_name_plural = _('Commercial Bonds')
+        verbose_name = _('Not Negotiable Futures')
+        verbose_name_plural = _('Not Negotiable Futures')
 
 
-class NotNegotiableCorporateBonds(NoneExtendedExtendedMarketableBonds):
-    activity_industry = models.CharField(_('Activity Industry'), max_length=255)
+class NotNegotiableStructuredProducts(Quarter, ExtendedMarketableBonds, MarketableBonds, RatedMarketableBonds,
+                                      PurchasableDateBonds):
+    base_asset = models.CharField(_('Base Asset'), max_length=255)
 
     class Meta:
-        verbose_name = _('Corporate Bonds')
-        verbose_name_plural = _('Corporate Bonds')
+        verbose_name = _('Not Negotiable Structured Products')
+        verbose_name_plural = _('Not Negotiable Structured Products')
 
 
-class LandRights(models.Model):
+class Loans(Quarter, Bonds, ExtendedMarketableBonds, RatedMarketableBonds):
+    market_value = models.DecimalField(_('Market Value'), help_text="Value is in thousands.",
+                                       max_digits=50, decimal_places=3)
+    investment_assets_rate = models.DecimalField(_('Investment Assets Rate'), help_text="This is a percentage value.",
+                                                 validators=[validate_percentage], max_digits=50, decimal_places=3)
+
+    class Meta:
+        verbose_name = _('Loans')
+        verbose_name_plural = _('Loans')
+
+
+class DepositsOverThreeMonths(Quarter, models.Model):
+    pass
+
+    class Meta:
+        verbose_name = _('Deposits Over Three Months')
+        verbose_name_plural = _('Deposits Over Three Months')
+
+
+class LandRights(Quarter, models.Model):
     pass
 
     class Meta:
         verbose_name = _('Land Rights')
-        verbose_name_plural = _('LandRights')
+        verbose_name_plural = _('Land Rights')
 
 
-class CompaniesInvestments(models.Model):
-    pass
-
-    class Meta:
-        verbose_name = _('Companies Investments')
-        verbose_name_plural = _('Companies Investments')
-
-
-class OtherInvestments(models.Model):
+class OtherInvestments(Quarter, models.Model):
     pass
 
     class Meta:
@@ -256,7 +240,7 @@ class OtherInvestments(models.Model):
         verbose_name_plural = _('Other Investments')
 
 
-class InvestmentsBalance(models.Model):
+class InvestmentsBalance(Quarter, models.Model):
     pass
 
     class Meta:
@@ -264,9 +248,29 @@ class InvestmentsBalance(models.Model):
         verbose_name_plural = _('Investments Balance')
 
 
-class Others(models.Model):
+class CostCoordinatedCorporateBonds(Quarter, MarketableBonds, RatedMarketableBonds, PurchasableDateBonds,
+                                    ActivityIndustryBonds):
+    average_life_span = models.DecimalField(_('Average Life Span'), max_digits=50, decimal_places=3)
+    effective_interest_rate = models.DecimalField(_('Effective Interest Rate'), max_digits=50, decimal_places=3)
+
+    class Meta:
+        verbose_name = _('Corporate Bonds')
+        verbose_name_plural = _('Corporate Bonds')
+
+
+class NotNegotiableCostCoordinatedCorporateBonds(Quarter, MarketableBonds, RatedMarketableBonds, PurchasableDateBonds,
+                                                 ActivityIndustryBonds):
+    average_life_span = models.DecimalField(_('Average Life Span'), max_digits=50, decimal_places=3)
+    effective_interest_rate = models.DecimalField(_('Effective Interest Rate'), max_digits=50, decimal_places=3)
+
+    class Meta:
+        verbose_name = _('Not Negotiable Corporate Bonds')
+        verbose_name_plural = _('Not Negotiable Corporate Bonds')
+
+
+class UnusedFramesBorrowers(Quarter, models.Model):
     pass
 
     class Meta:
-        verbose_name = _('Others')
-        verbose_name_plural = _('Others')
+        verbose_name = _('Unused Frames Borrowers')
+        verbose_name_plural = _('Unused Frames Borrowers')
