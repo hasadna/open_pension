d810cf0737ca043f534fbe610b4110c2c80efb89
nirgn975 <nir@galon.io>
2017-02-20 20:01:03 +0200

    Add intro module

---
+++ client/src/app/modules/intro/intro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'op-intro',
+  template: ``,
+  styles: [``]
+})
+export class IntroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/modules/intro/intro.module.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import IntroModule from './intro.module';
+
+describe('IntroModule', () => {
+  let introModule;
+
+  beforeEach(() => {
+    introModule = new IntroModule();
+  });
+
+  it('should create an instance', () => {
+    expect(introModule).toBeTruthy();
+  })
+});
