d00b7794687d95e8cef594169f852d8bdb6f7589
nirgn975 <nir@galon.io>
2017-03-19 22:08:12 +0200

    Cleanup

--- client/src/app/components/contact/contact.component.ts
+++ client/src/app/components/contact/contact.component.ts
@@ -1,42 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'op-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent implements OnInit, OnChanges {
   contactForm: FormGroup;
   nameControl;
   emailControl;
   contentControl;
   displayErrs: boolean = false;
 
-
-  constructor(private formBuilder: FormBuilder) {
-      this.buildForm();
-  }
-
-  checkAll(){
-      if(!this.contactForm.valid) {
-          this.displayErrs = true;
-      }
-  }
-  buildForm() {
-      this.contactForm = this.formBuilder.group({
-          name: this.formBuilder.control(null, [Validators.minLength(2),
-                                                Validators.required]),
-          email: this.formBuilder.control(null, [Validators.pattern(/\S+@\S+\.\S+/),
-                                                Validators.required]),
-          content: this.formBuilder.control(null, Validators.required),
-      });
-      this.nameControl = this.contactForm.get('name');
-      this.emailControl = this.contactForm.get('email');
-      this.contentControl = this.contactForm.get('content');
-  }
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+      name: this.formBuilder.control(null, [Validators.minLength(2), Validators.required]),
+      email: this.formBuilder.control(null, [Validators.pattern(/\S+@\S+\.\S+/), Validators.required]),
+      content: this.formBuilder.control(null, Validators.required),
+    });
+
+    this.nameControl = this.contactForm.get('name');
+    this.emailControl = this.contactForm.get('email');
+    this.contentControl = this.contactForm.get('content');
   }
 
+  checkAll() {
+    console.log('this.contactForm.valid', this.contactForm.valid);
+    if(!this.contactForm.valid) {
+      this.displayErrs = true;
+    }
+  }
+
+  onSubmit() {
+    const formModel = this.contactForm.value;
+    console.log('save', formModel);
+    this.ngOnChanges();
+  }
+
+  ngOnChanges() {
+    this.contactForm.setValue({
+      name: '',
+      email: '',
+      content: ''
+    });
+  }
 }
