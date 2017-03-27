c0a7874dbde95f0ec98e7cd3301cff00525042ed
RoySegall <roy.segall@gizra.com>
2017-03-27 20:04:03 +0300

    Adding field.

--- server/pension/management/commands/parse_csv.py
+++ server/pension/management/commands/parse_csv.py
@@ -104,6 +104,7 @@ class Command(BaseCommand):
         "תל אביב 75": "tlv_75",
         "מניות היתר": "extra_stocks",
         "call 001 אופציות": "options_001",
+        "אגח קונצרני של חברות זרות": "concern_bond_of_foreign_countries",
     }
 
     global_contexts = {
