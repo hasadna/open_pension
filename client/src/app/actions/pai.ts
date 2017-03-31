025414fd428a9ae79749d5035e81dec11b383a2f
nirgn975 <nir@galon.io>
2017-03-31 10:16:29 +0300

    Cleanup

--- client/src/app/actions/pai.ts
+++ client/src/app/actions/pai.ts
@@ -1,25 +1,8 @@
 import { Action } from '@ngrx/store';
 import { Pai } from '../models/pai';
 
-// export const ActionTypes = {
-//   LOAD_PAI:                type('[PAI] Load Pai'),
-//   LOAD_PAI_SUCCESS:        type('[PAI] Load Pai Success'),
-// };
-
 export const LOAD_PAI =  '[PAI] Load Pai';
 export const LOAD_PAI_SUCCESS = '[PAI] Load Pai Success';
-
-// export class LoadPaiAction implements Action {
-//   type = ActionTypes.LOAD_PAI;
-//
-//   constructor() { }
-// }
-//
-// export class LoadPaiSuccessAction implements Action {
-//   type = ActionTypes.LOAD_PAI_SUCCESS;
-//
-//   constructor(public payload: Pai) { }
-// }
 
 export class LoadPaiAction implements Action {
   readonly type = LOAD_PAI;
@@ -32,7 +15,6 @@ export class LoadPaiSuccessAction implements Action {
 
   constructor(public payload: Pai) { }
 }
-
 
 export type Actions
   = LoadPaiAction
--- client/src/app/modules/blog/actions/post.ts
+++ client/src/app/modules/blog/actions/post.ts
@@ -1,57 +1,29 @@
 import { Action } from '@ngrx/store';
 import { Post } from '../models/post';
 
-// export const ActionTypes = {
-//   LOAD_POSTS:                type('[POST] Load Posts'),
-//   LOAD_POSTS_SUCCESS:        type('[POST] Load Posts Success'),
-//   LOAD_POST_BY_ID:           type('[POST] Load Post By Id'),
-//   LOAD_POST_BY_ID_SUCCESS:   type('[POST] Load Posts By Id Success'),
-// };
-
 export const LOAD_POSTS =  '[POST] Load Posts';
 export const LOAD_POSTS_SUCCESS = '[POST] Load Posts Success';
 export const LOAD_POST_BY_ID =  '[POST] Load Post By Id';
 export const LOAD_POST_BY_ID_SUCCESS = '[POST] Load Posts By Id Success';
-//
-// export class LoadPostsAction implements Action {
-//   type = ActionTypes.LOAD_POSTS;
-//
-//   constructor() { }
-// }
-//
-// export class LoadPostsSuccessAction implements Action {
-//   type = ActionTypes.LOAD_POSTS_SUCCESS;
-//
-//   constructor(public payload: Post[]) { }
-// }
-//
-// export class LoadPostByIdAction implements Action {
-//   type = ActionTypes.LOAD_POST_BY_ID;
-//
-//   constructor(public payload: string) { }
-// }
-// 
-// export class LoadPostByIdSuccessAction implements Action {
-//   type = ActionTypes.LOAD_POST_BY_ID_SUCCESS;
-//
-//   constructor(public payload: any) { }
-// }
 
 export class LoadPostsAction implements Action {
   readonly type = LOAD_POSTS;
 
   constructor() { }
 }
+
 export class LoadPostsSuccessAction implements Action {
   readonly type = LOAD_POSTS_SUCCESS;
 
   constructor(public payload: Post[]) { }
 }
+
 export class LoadPostByIdAction implements Action {
   readonly type = LOAD_POST_BY_ID;
 
   constructor(public payload: string) { }
 }
+
 export class LoadPostByIdSuccessAction implements Action {
   readonly type = LOAD_POST_BY_ID_SUCCESS;
 
