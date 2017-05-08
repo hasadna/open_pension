ee076707e0f4dd5be8f134109b1ff4d4b591bae9
nirgn975 <nir@galon.io>
2017-05-08 23:15:33 +0300

    Fix services

--- client/src/app/services/pai.service.spec.ts
+++ client/src/app/services/pai.service.spec.ts
@@ -1,11 +1,25 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpModule, Http, BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
 import { PaiService } from './pai.service';
 
 describe('PaiService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PaiService]
+      imports: [ HttpModule ],
+      providers: [
+        PaiService,
+        {
+          provide: Http,
+          useFactory: (mockBackend, options) => {
+            return new Http(mockBackend, options);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        MockBackend,
+        BaseRequestOptions,
+      ]
     });
   });
 
