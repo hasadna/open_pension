b30cd37bb060c3d56ebb995125b5bf1848c9ba8c
RoySegall <roy.segall@gizra.com>
2017-06-08 22:09:49 +0300

    Adding a small test.

---
+++ server/pension/management/commands/test_parse_csv.py
@@ -0,0 +1,16 @@
+import unittest
+from parse_csv import Command
+
+
+class TestParseCSV(unittest.TestCase):
+
+    def test_should_skip_line(self):
+        self.assertFalse(Command.should_skip_line(self, 'foo'))
+        self.assertTrue(Command.should_skip_line(self, 'בעל ענין/צד קשור *'))
+        self.assertTrue(Command.should_skip_line(self, 'dasdasd בעל ענין/צד קשור *'))
+        self.assertTrue(Command.should_skip_line(self, ''))
+        self.assertFalse(Command.should_skip_line(self, 'Testing'))
+
+
+if __name__ == '__main__':
+    unittest.main()
