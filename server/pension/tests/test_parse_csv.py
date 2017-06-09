494463f6f8bbb5acfcbba81714cdeffc9e3fea97
RoySegall <roy.segall@gizra.com>
2017-06-09 14:35:21 +0300

    try to fail.

--- server/pension/tests/test_parse_csv.py
+++ server/pension/tests/test_parse_csv.py
@@ -8,7 +8,7 @@ from server.pension.management.commands.parse_csv import Command
 class TestParseCSV(unittest.TestCase):
 
     def test_should_skip_line(self):
-        self.assertFalse(Command.should_skip_line(self, 'foo'))
+        self.assertTrue(Command.should_skip_line(self, 'foo'))
         self.assertTrue(Command.should_skip_line(self, 'בעל ענין/צד קשור *'))
         self.assertTrue(Command.should_skip_line(self, 'dasdasd בעל ענין/צד קשור *'))
         self.assertTrue(Command.should_skip_line(self, ''))
