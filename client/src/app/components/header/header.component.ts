ab25ccb5417cde271ea9209a476e487763109fd2
savyoncohen <savyon.cohen@gmail.com>
2017-07-24 21:19:18 +0300

    add intro js

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -9,9 +9,47 @@ declare const introJs: any;
 })
 export class HeaderComponent {
 
-  constructor() { }
+  constructor() { this.intro() }
 
   intro() {
+    console.log('a');
+    var intro = introJs();
+    intro.setOptions({
+      nextLabel : "המשך >>",
+      prevLabel : "",
+      skipLabel : "",
+      doneLabel : "סגור",
+      steps : [
+        {
+          intro: "Hello world!"
+        },
+        {
+          element: document.querySelector('.filters-button.button-lightblue'),
+          intro: "This is a tooltip.",
+          position: "left"
+        },
+        {
+          element: document.querySelectorAll('#step2')[0],
+          intro: "Ok, wasn't that fun?",
+          position: 'right'
+        },
+        {
+          element: '#step3',
+          intro: 'More features, more fun.',
+          position: 'left'
+        },
+        {
+          element: '#step4',
+          intro: "Another step.",
+          position: 'bottom'
+        },
+        {
+          element: '#step5',
+          intro: 'Get it, use it.'
+        }
+      ]
+    });
+    introJs().setOptions('nextLabel', 'foo');
     introJs().start();
   }
 }
