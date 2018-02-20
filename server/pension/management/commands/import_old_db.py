2a9b973c42723aafb4b09367e5a23a67cef9a51c
nirgn975 <nir@galon.io>
2018-02-20 14:49:01 +0200

    Import old data, pension_data_all table

--- server/pension/management/commands/import_old_db.py
+++ server/pension/management/commands/import_old_db.py
@@ -1,7 +1,8 @@
 import pandas as pd
+from os import path
 
 from django.core.management import BaseCommand
-from pension.models import Quarter, Instrument
+from pension.models import Quarter, Fund
 
 managing_body_dict = {
     'amitim': 'AMT',
@@ -123,15 +124,35 @@ instrument_type_dict = {
 
 
 class Command(BaseCommand):
+    def value_or_empty(self, value):
+        if str(value) == 'nan':
+            return None
+        return value
+
+    def get_float_or_none(self, value):
+        try:
+            return float(value)
+        except (TypeError, ValueError) as e:
+            return None
+
+    def get_int_or_none(self, value):
+        try:
+            return int(value)
+        except (TypeError, ValueError) as e:
+            return None
+
     def handle(self, *args, **options):
         print('Importing..')
-        filename = 'pension_data_all.csv'
+        BASE_DIR = path.dirname(path.dirname(path.abspath(__file__)))
+        DATA_ROOT = path.abspath(path.join(BASE_DIR, '../../data'))
+        csv_filename = path.join(DATA_ROOT, 'pension_data_all.csv')
+
         data_csv = pd.read_csv(
-            '/Users/nirgalon/Downloads/{filename}'.format(filename=filename),
+            csv_filename,
             index_col=False,
             header=0,
         )
-        print('Load the file')
+        print('Start to load the file')
 
         # Read the content of the sheet
         for index, managing_body in enumerate(data_csv['managing_body']):
@@ -140,77 +161,62 @@ class Command(BaseCommand):
                 month=data_csv['report_qurater'][index],
             )
 
-            if str(data_csv['date_of_revaluation'][index]) == 'nan':
-                date_of_revaluation = ''
+            if not self.value_or_empty(data_csv['instrument_sub_type'][index]):
+                instrument_sub_type = ''
             else:
-                date_of_revaluation = data_csv['date_of_revaluation'][index]
+                instrument_sub_type = instrument_sub_type_dict[data_csv['instrument_sub_type'][index]]
 
             try:
                 rating = rating_dict[data_csv['rating'][index]]
             except (KeyError, TypeError):
                 rating = 'לא דווח'
 
-            if str(data_csv['underlying_asset'][index]) == 'nan':
-                underlying_asset = ''
+            if str(data_csv['dual_trade'][index]) == 't':
+                dual_trade = True
             else:
-                underlying_asset = data_csv['underlying_asset'][index]
+                dual_trade = False
 
-            if str(data_csv['date_of_purchase'][index]) == 'nan':
-                date_of_purchase = ''
-            else:
-                date_of_purchase = data_csv['date_of_purchase'][index]
-
-            if str(data_csv['type_of_asset'][index]) == 'nan':
-                type_of_asset = ''
-            else:
-                type_of_asset = data_csv['type_of_asset'][index]
-
-            if str(data_csv['instrument_sub_type'][index]) == 'nan':
-                instrument_sub_type = ''
-            else:
-                instrument_sub_type = instrument_sub_type_dict[data_csv['instrument_sub_type'][index]]
-
-            instrument, created = Instrument.objects.get_or_create(
+            fund, created = Fund.objects.get_or_create(
+                managing_body=managing_body_dict[managing_body],
+                fund=self.get_int_or_none(data_csv['fund'][index]),
+                fund_name=self.value_or_empty(data_csv['fund_name'][index]),
+                quarter=quarter[0],
                 instrument_type=instrument_type_dict[data_csv['instrument_type'][index]],
                 instrument_sub_type=instrument_sub_type,
-                # instrument_id='',
-                fund_name=data_csv['fund_name'][index],
-                fund_id=data_csv['fund'][index],
-                issuer_id=data_csv['instrument_id'][index],  # regex from Oded. and replace to instrument_sub_type.
-                issuer_name=data_csv['instrument_name'][index],
+                instrument_id=self.get_int_or_none(data_csv['instrument_id'][index]),
+                orig_instrument_id=self.value_or_empty(data_csv['orig_instrument_id'][index]),
+                instrument_name=self.value_or_empty(data_csv['instrument_name'][index]),
+                issuer=self.value_or_empty(data_csv['issuer'][index]),
+                activity_industry=self.value_or_empty(data_csv['activity_industry'][index]),
+                currency=self.value_or_empty(data_csv['currency'][index]),
+                fair_value=self.get_float_or_none(data_csv['fair_value'][index]),
+                market_cap=self.get_float_or_none(data_csv['market_cap'][index]),
+                rate_of_fund=self.get_float_or_none(data_csv['rate_of_fund'][index]),
+                rating_agency=self.value_or_empty(data_csv['rating_agency'][index]),
+                reference_index=self.value_or_empty(data_csv['reference_index'][index]),
+                intrest_rate=self.get_float_or_none(data_csv['intrest_rate'][index]),
+                date_of_purchase=self.value_or_empty(data_csv['date_of_purchase'][index]),
+                average_of_duration=self.get_float_or_none(data_csv['average_of_duration'][index]),
+                date_of_revaluation=self.value_or_empty(data_csv['date_of_revaluation'][index]),
+                rate=self.get_float_or_none(data_csv['rate'][index]),
+                yield_to_maturity=self.get_float_or_none(data_csv['yield'][index]),
                 rating=rating,
-                rating_agency=data_csv['rating_agency'][index],
-                currency=data_csv['currency'][index],
-                interest_rate=data_csv['intrest_rate'][index],
-                yield_to_maturity=data_csv['yield'][index],
-                par_value=data_csv['par_value'][index],  # fair value
-                # market_cap=''  # estimated_value = fair_value = market_cap
-                # rate_of_investment_channel='',
-                rate_of_fund=data_csv['rate_of_fund'][index],
-                # trading_floor='',
-                date_of_purchase=date_of_purchase,
-                average_of_duration=data_csv['average_of_duration'][index],
-                rate=data_csv['rate'][index],
-                rate_of_ipo=data_csv['rate_of_ipo'][index],
-                # informer='',
-                fair_value=data_csv['fair_value'][index],
-                activity_industry=data_csv['activity_industry'][index],
-                date_of_revaluation=date_of_revaluation,
-                type_of_asset=type_of_asset,
-                # rate_of_return_during_period='',
-                # return_on_equity='',
-                # liabilities='',
-                # expiry_date_of_liabilities='',
-                # effective_rate='',
-                # coordinated_cost='',
-                underlying_asset=underlying_asset,
-                # consortium='',
-                # average_rate='',
-                # estimated_value='',  # estimated_value = fair_value = market_cap
-                managing_body=managing_body_dict[managing_body],
-                # geographical_location='',
-                # negotiable='',
-                quarter=quarter[0],
+                par_value=self.get_float_or_none(data_csv['par_value'][index]),
+                underlying_asset=self.value_or_empty(data_csv['underlying_asset'][index]),
+                type_of_asset=self.value_or_empty(data_csv['type_of_asset'][index]),
+                rate_of_ipo=self.get_float_or_none(data_csv['rate_of_ipo'][index]),
+                liquidity=self.value_or_empty(data_csv['liquidity'][index]),
+                asset_type=self.value_or_empty(data_csv['asset_type'][index]),
+                row_cleansing_time=self.value_or_empty(data_csv['row_cleansing_time'][index]),
+                issuer_number=self.get_int_or_none(data_csv['issuer_number'][index]),
+                owner_option=self.value_or_empty(data_csv['owner_option'][index]),
+                original_issuer_number=self.get_int_or_none(data_csv['original_issuer_number'][index]),
+                isin=self.value_or_empty(data_csv['isin'][index]),
+                instrument_symbol=self.value_or_empty(data_csv['instrument_symbol'][index]),
+                movil=self.value_or_empty(data_csv['movil'][index]),
+                sector=self.value_or_empty(data_csv['sector'][index]),
+                dual_trade=dual_trade,
+                cleansing_action=self.get_int_or_none(data_csv['cleansing_action'][index]),
             )
             print('Finish to import {index} row!'.format(index=index))
 
