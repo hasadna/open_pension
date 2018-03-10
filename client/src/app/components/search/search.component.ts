30bbd182cdecc454be43c4eccde7d701c3ae020c
David Bronfen <davidbronfen@gmail.com>
2018-03-10 13:21:59 +0200

    Add search submit and search state subscription.

--- client/src/app/components/search/search.component.ts
+++ client/src/app/components/search/search.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import * as fromRoot from '../../reducers';
+import { SearchResult } from '../../models/search.model'
+import { SearchAction } from '../../actions/search.actions';
+
 
 @Component({
   selector: 'op-search',
@@ -7,9 +15,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  public searchResult$: Observable<SearchResult[]>;
 
-  ngOnInit() {
+  constructor(private store: Store<fromRoot.State>) {
+    this.searchResult$ = store.select(fromRoot.getSearchState)
   }
 
+  ngOnInit() {
+    this.form = new FormGroup({
+      'searchInput': new FormControl('', []),
+    });
+  }
+
+  onSubmit() {
+    const encodedTerm = encodeURI(this.form.value.searchInput);
+    this.store.dispatch(new SearchAction(encodedTerm));
+
+    console.log(this.searchResult$);
+  }
 }
