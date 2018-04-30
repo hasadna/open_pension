c0632a01db1511aa62360a116b5befd171da49cd
David Bronfen <davidbronfen@gmail.com>
2018-04-30 16:40:10 +0300

    Added search progress to the result subscription.

--- client/src/app/components/search/search.component.ts
+++ client/src/app/components/search/search.component.ts
@@ -3,7 +3,7 @@ import { FormGroup, FormControl } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { SlicePipe } from '@angular/common';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 import * as fromRoot from '../../reducers';
 import { SearchResult } from '../../models/search.model';
@@ -20,26 +20,30 @@ export class SearchComponent implements OnInit {
   form: FormGroup;
   public searchResults: SearchResult[];
   public searchSubmitted = false;
+  public showProgressBar = false;
 
   constructor(private store: Store<fromRoot.State>, private router: Router) {
     store.select(fromRoot.getSearchedEntities).subscribe(
       res => {
-        this.searchResults = res;
-        if (res[0].pk !== 0) {
-           this.searchSubmitted = true;
+        if (res.length > 0) {
+          this.searchResults = res;
+          if (res[0].pk !== 0) {
+             this.searchSubmitted = true;
+             this.showProgressBar = false;
+          }
         }
       }
     );
   }
 
   ngOnInit() {
-    console.log(this.searchResults);
     this.form = new FormGroup({
       'searchInput': new FormControl('', []),
     });
   }
 
   onSubmit() {
+    this.showProgressBar = true;
     const encodedTerm = encodeURI(this.form.value.searchInput);
     this.store.dispatch(new SearchAction(encodedTerm));
   }
@@ -55,5 +59,6 @@ export class SearchComponent implements OnInit {
     const encodedUri = encodeURI(item.fields.fund_name);
     this.router.navigate([`detail-pai/${encodedUri}`]);
     this.searchSubmitted = false;
+    this.showProgressBar = false;
   }
 }
