33c899274e31f30299a58a4ae7547d2a234b792c
Itamar Gronich <itamargronich@gmail.com>
2018-06-04 20:32:29 +0300

    Basic docker infra for dev and prod (#351)
    * [WIP] Started docker for dev. Basic config & env files.
    
    * [WIP] start utilizing common.
    
    * Utilize .env.prod file.
    
    * [WIP] Add docker-compose.dev.yml file.
    
    * [WIP] finalize docker-compose.dev.yml
    
    * initial setup with docker-compose. server is up and running. What's left is \- automatic migration
    
    * Add volume mount for client service.
    
    * Script to automate build process.
    
    * Route all volumes, expose DB to localhost.
    
    * Make the switch to small containers.
    Start working with docker hub.
    
    * Server image now being built.
    Still need to run it tho
    
    * Preventing node_modules from being sent to daemon context.
    
    * Basic docker compose working.
    
    * Checkout package-lock.json from master
    
    * Checkout entrypoint from master
    
    * Define ports and proxy from client.
    
    * Add support for ipv6
    
    * Setting up different envs and logging. 👍🏻
    
    * Add maintainer tags 👎🏻
    
    * Wait for db to be available.
    
    * Builder pattern.
    
    * Copy internal folder structure.
    
    * Create django log in travis
    
    * Better server install script.
    
    * Typo
    
    * Make folder logs and then create log file.
    
    * Woops.

--- server/config/settings.py
+++ server/config/settings.py
@@ -95,10 +95,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME', ''),
-        'USER': os.environ.get('DB_USER', ''),
-        'PASSWORD': os.environ.get('DB_PASS', ''),
-        'HOST': os.environ.get('DB_HOST', ''),
+        'NAME': os.environ.get('POSTGRES_DB', ''),
+        'USER': os.environ.get('POSTGRES_USER', ''),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+        'HOST': os.environ.get('POSTGRES_HOST', ''),
         'PORT': 5432,
     }
 }
@@ -123,6 +123,25 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Logging
+LOG_LEVEL = 'DEBUG' if os.environ.get("ENV") == "DEV" else "ERROR"
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': LOG_LEVEL,
+            'class': 'logging.FileHandler',
+            'filename': '/home/app/server/logs/django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': LOG_LEVEL,
+            'propagate': True,
+        },
+    },
+}
 
 RAVEN_CONFIG = {
     'dsn': 'https://2d4c5f09376d40ef8beef9b4b5444667:1d6e71bd3c74485dab6529b6dee9bd59@sentry.io/202882',
--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -3,6 +3,14 @@
 echo "[run] go to project folder"
 cd /home/app/server
 
+# TODO - better to have a more robust way to do this
+echo "Waiting for DB connection"
+while ! echo exit | nc database 5432;
+do
+    echo "DB still off-line....";
+    sleep 10;
+done
+
 echo "[run] Migrate DB"
 python manage.py migrate
 
@@ -14,3 +22,6 @@ echo "from django.contrib.auth.models import User
 if not User.objects.filter(username='admin').count():
     User.objects.create_superuser('admin', 'admin@example.com', 'pass')
 " | python manage.py shell
+
+echo "[run] Starting Up server Django at 80"
+gunicorn config.wsgi -b 0.0.0.0:80
