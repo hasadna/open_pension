0d5a6222fc791a4352aa998da497c79475a8834b
David Bronfen <davidbronfen@gmail.com>
2018-12-19 20:06:40 +0200

    Update quarters effects to use the nnew effects structure.

--- client/src/app/effects/quarters.effect.ts
+++ client/src/app/effects/quarters.effect.ts
@@ -1,7 +1,8 @@
 import { Action } from '@ngrx/store';
 import { of ,  Observable } from 'rxjs';
+import { switchMap, map, catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { Effect, Actions } from '@ngrx/effects';
+import { Effect, Actions, ofType } from '@ngrx/effects';
 
 import { PaiService } from '../services/pai.service';
 import { QuartersService } from '../services/quarters.service';
@@ -23,18 +24,24 @@ export class QuartersEffect {
   ) { }
 
   @Effect()
-  loadQuarters$: Observable<Action> = this.actions$
-    .ofType<LoadQuartersAction>(QuarterActionTypes.LOAD_QUARTERS)
-    .switchMap(_ => this.quartersService.getQuarters()
-      .map(quartersData => new LoadQuartersSuccessAction(quartersData))
-      .catch(() => of({ type: 'LOAD_QUARTERS_FAILED' }))
-    );
+  loadQuarters$: Observable<Action> = this.actions$.pipe(
+    ofType<LoadQuartersAction>(QuarterActionTypes.LOAD_QUARTERS),
+    switchMap(_ => this.quartersService.getQuarters()
+      .pipe(
+        map(quartersData => new LoadQuartersSuccessAction(quartersData)),
+        catchError(() => of({ type: 'LOAD_QUARTERS_FAILED' }))
+      )
+    )
+  );
 
   @Effect()
-  loadPaiAfterNewQuarter$: Observable<Action> = this.actions$
-    .ofType<SelectNewQuarterAction>(QuarterActionTypes.SELECT_NEW_QUARTER_ACTION)
-    .switchMap(_ => this.paiService.getPaiWithFilters()
-      .map(paiData => new LoadPaiAfterNewFilterSuccessAction(paiData))
-      .catch(() => of({ type: 'LOAD_PAI_AFTER_NEW_FILTER_FAILED' }))
-    );
+  loadPaiAfterNewQuarter$: Observable<Action> = this.actions$.pipe(
+    ofType<SelectNewQuarterAction>(QuarterActionTypes.SELECT_NEW_QUARTER_ACTION),
+    switchMap(_ => this.paiService.getPaiWithFilters()
+      .pipe(
+        map(paiData => new LoadPaiAfterNewFilterSuccessAction(paiData)),
+        catchError(() => of({ type: 'LOAD_PAI_AFTER_NEW_FILTER_FAILED' }))
+      )
+    )
+  );
 }
