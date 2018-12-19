5ed3cdf07544776b5307b4003de4792ab8bd303c
David Bronfen <davidbronfen@gmail.com>
2018-12-19 19:59:01 +0200

    Update pai effects to use the new effects structure.

--- client/src/app/effects/pai.effect.ts
+++ client/src/app/effects/pai.effect.ts
@@ -1,7 +1,8 @@
 import { Action } from '@ngrx/store';
 import { of ,  Observable } from 'rxjs';
+import { switchMap, map, catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { Effect, Actions } from '@ngrx/effects';
+import { Effect, Actions, ofType } from '@ngrx/effects';
 
 import { PaiService } from '../services/pai.service';
 import {
@@ -26,34 +27,46 @@ export class PaiEffect {
   ) { }
 
   @Effect()
-  loadPai$: Observable<Action> = this.actions$
-    .ofType<LoadPaiAction>(PaiActionTypes.LOAD_PAI)
-    .switchMap(_ => this.paiService.getPai()
-      .map(paiData => new LoadPaiSuccessAction(paiData))
-      .catch(() => of({ type: 'LOAD_PAI_FAILED' }))
-    );
+  loadPai$: Observable<Action> = this.actions$.pipe(
+    ofType<LoadPaiAction>(PaiActionTypes.LOAD_PAI),
+    switchMap(_ => this.paiService.getPai()
+      .pipe(
+        map(paiData => new LoadPaiSuccessAction(paiData)),
+        catchError(() => of({ type: 'LOAD_PAI_FAILED' }))
+      )
+    )
+  );
 
   @Effect()
-  loadPaiAfterNewFilter$: Observable<Action> = this.actions$
-    .ofType<SelectNewFilterAction>(FiltersActionTypes.SELECT_NEW_FILTER_ACTION)
-    .switchMap(_ => this.paiService.getPaiWithFilters()
-      .map(paiData => new LoadPaiAfterNewFilterSuccessAction(paiData))
-      .catch(() => of({ type: 'LOAD_PAI_AFTER_NEW_FILTER_FAILED' }))
-    );
+  loadPaiAfterNewFilter$: Observable<Action> = this.actions$.pipe(
+    ofType<SelectNewFilterAction>(FiltersActionTypes.SELECT_NEW_FILTER_ACTION),
+    switchMap(_ => this.paiService.getPaiWithFilters()
+      .pipe(
+        map(paiData => new LoadPaiAfterNewFilterSuccessAction(paiData)),
+        catchError(() => of({ type: 'LOAD_PAI_AFTER_NEW_FILTER_FAILED' }))
+      )
+    )
+  );
 
   @Effect()
-  loadPaiAfterRemoveFilter$: Observable<Action> = this.actions$
-    .ofType<RemoveSelectedFilterAction>(FiltersActionTypes.REMOVE_SELECTED_FILTER_ACTION)
-    .switchMap(_ => this.paiService.getPaiWithFilters()
-      .map(paiData => new LoadPaiAfterNewFilterSuccessAction(paiData))
-      .catch(() => of({ type: 'LOAD_PAI_AFTER_NEW_FILTER_FAILED' }))
-    );
+  loadPaiAfterRemoveFilter$: Observable<Action> = this.actions$.pipe(
+    ofType<RemoveSelectedFilterAction>(FiltersActionTypes.REMOVE_SELECTED_FILTER_ACTION),
+    switchMap(_ => this.paiService.getPaiWithFilters()
+      .pipe(
+        map(paiData => new LoadPaiAfterNewFilterSuccessAction(paiData)),
+        catchError(() => of({ type: 'LOAD_PAI_AFTER_NEW_FILTER_FAILED' }))
+      )
+    )
+  );
 
   @Effect()
-  changeFilterLayer$: Observable<Action> = this.actions$
-    .ofType<ChangeLayerOfFilterAction>(FiltersActionTypes.CHANGE_LAYER_OF_FILTER_ACTION)
-    .switchMap(_ => this.paiService.getPaiWithFilters()
-      .map(paiData => new LoadPaiAfterNewFilterSuccessAction(paiData))
-      .catch(() => of({ type: 'LOAD_PAI_AFTER_NEW_FILTER_FAILED' }))
-    );
+  changeFilterLayer$: Observable<Action> = this.actions$.pipe(
+    ofType<ChangeLayerOfFilterAction>(FiltersActionTypes.CHANGE_LAYER_OF_FILTER_ACTION),
+    switchMap(_ => this.paiService.getPaiWithFilters()
+      .pipe(
+        map(paiData => new LoadPaiAfterNewFilterSuccessAction(paiData)),
+        catchError(() => of({ type: 'LOAD_PAI_AFTER_NEW_FILTER_FAILED' }))
+      )
+    )
+  );
 }
