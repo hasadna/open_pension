d5964a0a4465c8138d0c3427f035feca6aecd0ae
David Bronfen <davidbronfen@gmail.com>
2019-03-29 14:38:23 +0300

    Ignore unreachable services.

--- gateway/src/app.ts
+++ gateway/src/app.ts
@@ -1,4 +1,4 @@
-import { ApolloServer, introspectSchema, makeRemoteExecutableSchema, mergeSchemas } from "apollo-server";
+import { ApolloServer, introspectSchema, makeExecutableSchema, makeRemoteExecutableSchema, mergeSchemas } from "apollo-server";
 
 import { HttpLink } from "apollo-link-http";
 import fetch from "node-fetch";
@@ -8,17 +8,19 @@ import { retry } from "./utils/asyncUtils";
 async function createRemoteSchema(serviceName) {
     const link = new HttpLink({uri: `http://${serviceName}`, fetch});
 
-    const schema = await retry(() => introspectSchema(link), 5);
-
-    return makeRemoteExecutableSchema({
-        schema,
-        link,
-    });
+    let schema;
+    try {
+        schema = await retry(() => introspectSchema(link), 5);
+        return makeRemoteExecutableSchema({ schema, link });
+    } catch (err) {
+        return undefined;
+    }
 }
 
 export async function createApolloServer() {
     const schemas = await Promise.all(["blog"].map(createRemoteSchema));
-    const mergedSchema = mergeSchemas({ schemas });
+    const filteredSchemas = schemas.filter((schema) => schema !== undefined);
+    const mergedSchema = mergeSchemas({ schemas: filteredSchemas });
     const app = new ApolloServer({schema: mergedSchema});
 
     // This `listen` method launches a web-server.  Existing apps
