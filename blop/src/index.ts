496a4add89ae8ce60d00af5801656f53766b7c1b
David Bronfen <davidbronfen@gmail.com>
2019-04-08 21:03:07 +0300

    Wrap init process in async function.

--- blop/src/index.ts
+++ blop/src/index.ts
@@ -1,14 +1,18 @@
 import { createApolloServer } from "./graphql";
 import { initSequelize } from "./sequelize";
 
-initSequelize();
-const apolloServer = createApolloServer();
+async function init() {
+    await initSequelize();
+    const apolloServer = createApolloServer();
 
-apolloServer.listen({ port: 80 }).then(({url}) => {
-    console.log(`🚀  Server ready at ${url}`);
-});
+    apolloServer.listen({port: 80}).then(({url}) => {
+        console.log(`🚀  Server ready at ${url}`);
+    });
 
+}
 process.on("SIGINT", async () => {
     console.log("Disconnecting");
     process.exit();
 });
+
+init();
