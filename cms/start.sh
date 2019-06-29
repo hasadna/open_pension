a8f3880a0bbb3ccee74bd19897777d0357369479
Roy Segall <roy@segall.io>
2019-06-29 23:10:11 +0300

    Adding the install.

--- cms/start.sh
+++ cms/start.sh
@@ -12,10 +12,14 @@ cd drupal
 composer install
 
 # Install drupal
-./vendor/bin/drupal site:install  standard  mysql://user:password@cms_db/db \
+./vendor/bin/drupal site:install open_pension mysql://user:password@cms_db/db \
   --langcode="en"  \
   --site-name="Drupal 8"  \
   --site-mail="admin@example.com"  \
   --account-name="admin"  \
   --account-mail="admin@example.com"  \
-  --account-pass="admin"
+  --account-pass="admin" \
+  --no-interaction \
+  --force
+
+echo "Completed\n"
