fb1fa8cdd44face02ba201fd022e969559bc1d8c
amitlicht <amitlicht@gmail.com>
2019-07-14 13:28:42 +0300

    convert to API class

--- Fetcher/sources/ami_gml.py
+++ Fetcher/sources/ami_gml.py
@@ -4,42 +4,40 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+from Fetcher.source_interface import SourceInterface
+
 base_url = "http://ami-gml.co.il"
 
-base_url_reports = "http://ami-gml.co.il/%d7%a8%d7%a9%d7%99%d7%9e%d7%aa-%d7%a0%d7%9b%d7%a1%d7%99%d7%9d-%d7%93%d7%95%d7%97%d7%95%d7%aa-%d7%9e%d7%90%d7%95%d7%97%d7%93%d7%99%d7%9d-{year}"
-
-report_text_base = "דוח נכסים רבעון"
-
-report_text_template = '{base} {quarter} {year}'
-
-save_path = '/tmp/reports'
+reports_relative_url = "%d7%a8%d7%a9%d7%99%d7%9e%d7%aa-%d7%a0%d7%9b%d7%a1%d7%99%d7%9d-%d7%93%d7%95%d7%97%d7%95%d7%aa-%d7%9e%d7%90%d7%95%d7%97%d7%93%d7%99%d7%9d-{year}"
 
 
-def get_ami_gml_quarterly(year, quarter):
+class AmiGmlSource(SourceInterface):
     """
     עמ"י - חברה לניהול קופות גמל ענפיות בע"מ
-    :param year:
-    :param quarter:
-    :return:
     """
-    base_url_yearly = base_url_reports.format(year=year)
-    r = requests.get(base_url_yearly)
-    parsed_html = BeautifulSoup(r.content)
+    def get_annual(self, year: int):
+        pass
 
-    items = parsed_html.find_all(href=re.compile(f'.*gsum_{quarter:02}{str(year)[-2:]}.*'))
-    if not items:
-        print(f"Failed finding report for {year}-{quarter}")
-        return
+    def get_quarterly(self, year: int, quarter: int):
+        base_url_yearly = base_url + '/' + reports_relative_url.format(year=year)
+        r = requests.get(base_url_yearly)
+        parsed_html = BeautifulSoup(r.content)
 
-    href = items[0]['href']
+        items = parsed_html.find_all(href=re.compile(f'.*gsum_{quarter:02}{str(year)[-2:]}.*'))
+        if not items:
+            print(f"Failed finding report for {year}-{quarter}")
+            return
 
-    d = requests.get(base_url + '/' + href)
+        href = items[0]['href']
 
-    file_path = os.path.join(save_path, f'{year}-{quarter}.xls')
-    with open(file_path, 'wb') as f:
-        f.write(d.content)
-        print(f'Saved file {href} to {file_path}')
+        d = requests.get(base_url + '/' + href)
+
+        file_path = os.path.join(self.output_path, f'{year}-{quarter}.xls')
+        with open(file_path, 'wb') as f:
+            f.write(d.content)
+            print(f'Saved file {href} to {file_path}')
 
 
 if __name__ == '__main__':
-    get_ami_gml_quarterly(2018, 1)
+    save_path = '/tmp/reports'
+    AmiGmlSource(save_path).get_quarterly(2018, 1)
--- Fetcher/sources/infinity_khr.py
+++ Fetcher/sources/infinity_khr.py
@@ -2,45 +2,47 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
+from Fetcher.source_interface import SourceInterface
+
 base_url = "http://infinity.co.il/nihulkupot/Kahar/%D7%A7-%D7%94-%D7%A8-%D7%A0%D7%9B%D7%A1%D7%99-%D7%94%D7%A7%D7%95%D7%A4%D7%94-%D7%95%D7%AA%D7%A9%D7%95%D7%90%D7%95%D7%AA/%D7%A7-%D7%94-%D7%A8-%D7%A8%D7%A9%D7%99%D7%9E%D7%AA-%D7%A0%D7%9B%D7%A1%D7%99%D7%9D-%D7%A8%D7%91%D7%A2%D7%95%D7%A0%D7%99%D7%AA/"
 
 report_text_base = "דוח נכסים רבעון"
 
 report_text_template = '{base} {quarter} {year}'
 
-save_path = '/tmp/reports'
 
-
-def get_khr_quarterly(year, quarter):
+class InfinityKHRSource(SourceInterface):
     """
     ק.ה.ר - קרן השתלמות לרוקחים בע"מ
-    :param year:
-    :param quarter:
-    :return:
     """
-    r = requests.get(base_url)
-    parsed_html = BeautifulSoup(r.content)
-    report_text_by_year_month = report_text_template.format(base=report_text_base,
-                                                            year=year,
-                                                            quarter=quarter)
-    items = parsed_html.find_all(text=report_text_by_year_month)
-    if not items:
-        print(f"Failed finding report for {year}-{quarter}")
-        return
+    def get_annual(self, year: int):
+        pass
 
-    p = items[0].parent
-    href = p['href']
+    def get_quarterly(self, year: int, quarter: int):
+        r = requests.get(base_url)
+        parsed_html = BeautifulSoup(r.content)
+        report_text_by_year_month = report_text_template.format(base=report_text_base,
+                                                                year=year,
+                                                                quarter=quarter)
+        items = parsed_html.find_all(text=report_text_by_year_month)
+        if not items:
+            print(f"Failed finding report for {year}-{quarter}")
+            return
 
-    d = requests.get(href)
+        p = items[0].parent
+        href = p['href']
 
-    file_path = os.path.join(save_path, f'{year}-{quarter}.xls')
-    with open(file_path, 'wb') as f:
-        f.write(d.content)
-        print(f'Saved file {href} to {file_path}')
+        d = requests.get(href)
+
+        file_path = os.path.join(self.output_path, f'{year}-{quarter}.xls')
+        with open(file_path, 'wb') as f:
+            f.write(d.content)
+            print(f'Saved file {href} to {file_path}')
 
 
 if __name__ == '__main__':
-    get_khr_quarterly(2017, 4)
+    save_path = '/tmp/reports'
+    InfinityKHRSource(save_path).get_quarterly(2017, 4)
 
 
 
