8c0d536a2534a3caa81300a65b74c37da2c8d06a
Eddie Linder <eddie@guardicore.com>
2019-07-14 11:22:48 +0300

    Create basic sources interface

---
+++ fetcher/source_interface.py
@@ -0,0 +1,14 @@
+from abc import ABCMeta, abstractmethod
+
+
+class SourceInterface(metaclass=ABCMeta):
+    def __init__(self, output_path: str):
+        self.output_path = output_path
+
+    @abstractmethod
+    def get_annual(self, year: int):
+        pass
+
+    @abstractmethod
+    def get_quarterly(self, year: int, quarter: int):
+        pass
