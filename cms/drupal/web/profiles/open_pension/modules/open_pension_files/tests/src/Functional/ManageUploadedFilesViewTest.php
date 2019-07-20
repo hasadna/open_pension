6ed702da9d45e6377d5fc13bc7566598b92629d5
Roy Segall <roy@segall.io>
2019-07-20 11:21:54 +0300

    Order code. Now, let’s finish the next test.

--- cms/drupal/web/profiles/open_pension/modules/open_pension_files/tests/src/Functional/ManageUploadedFilesViewTest.php
+++ cms/drupal/web/profiles/open_pension/modules/open_pension_files/tests/src/Functional/ManageUploadedFilesViewTest.php
@@ -49,11 +49,6 @@ class ManageUploadedFilesViewTest extends BrowserTestBase {
   protected $fileSystem;
 
   /**
-   * @var OpenPensionFilesProcessInterface
-   */
-  protected $openPensionFilesProcess;
-
-  /**
    * @var Media[]
    */
   protected $mediaObjects;
@@ -62,8 +57,22 @@ class ManageUploadedFilesViewTest extends BrowserTestBase {
    * @var array
    */
   protected $files = [
-    'dummy-xsl-file.xlsx',
-    'dummy-xsl-file-2.xlsx'
+    'dummy-xsl-file.xlsx' => [
+      'media_object_key' => 0,
+      'after_process_text' => 'Yes',
+      'logs' => [
+        'The file dummy-xsl-file.xlsx has been processed',
+        'Starting to process the file dummy-xsl-file.xlsx',
+      ],
+    ],
+    'dummy-xsl-file-2.xlsx' => [
+      'media_object_key' => 1,
+      'after_process_text' => 'No',
+      'logs' => [
+        "The file dummy-xsl-file-2.xlsx was not able to process",
+        "Starting to process the file dummy-xsl-file-2.xlsx",
+      ],
+    ]
   ];
 
   /**
@@ -77,7 +86,6 @@ class ManageUploadedFilesViewTest extends BrowserTestBase {
     // Setting up permissions and services.
     $this->account = $this->drupalCreateUser(['upload files', 'manage file']);
     $this->fileSystem = $this->container->get('file_system');
-    $this->openPensionFilesProcess = $this->container->get('open_pension_files.file_process');
 
     // Creating the services.
     $this->createFiles();
@@ -89,7 +97,7 @@ class ManageUploadedFilesViewTest extends BrowserTestBase {
   protected function createFiles() {
     $base_xsl_files = drupal_get_path('module', 'open_pension_files') . '/tests/src/Functional';
 
-    foreach ($this->files as $file) {
+    foreach (array_keys($this->files) as $file) {
       // Create the file object.
       $file_path = $this->fileSystem->realpath($base_xsl_files . '/' . $file);
 
@@ -123,22 +131,18 @@ class ManageUploadedFilesViewTest extends BrowserTestBase {
     // Check the files exists.
     $this->drupalGet('admin/open_pension/uploaded_files');
 
-    // Check we have two files.
-    $this->assertText($this->files[0]);
-    $this->assertText($this->files[1]);
+    // Check we have two files and make sure they have not been processed yet.
+    foreach ($this->files as $file_name => $file_metadata) {
+      $this->assertText($file_name);
 
-    // Check the first file was not processed yet.
-    $xpath = "//tr[@class='media-{$this->mediaObjects[0]->id()}']//td[contains(@class, 'processed') and contains(., 'No')]";
+      $media_object = $this->mediaObjects[$file_metadata['media_object_key']];
 
-    if (!$this->getSession()->getPage()->find("xpath", $xpath)) {
-      $this->fail("The file {$this->mediaObjects[0]->getName()} is processed before the related operation triggered.");
-    }
+      // Check the first file was not processed yet.
+      $xpath = "//tr[@class='media-{$media_object->id()}']//td[contains(@class, 'processed') and contains(., 'No')]";
 
-    // Check the second file was not processed yet.
-    $xpath = "//tr[@class='media-{$this->mediaObjects[1]->id()}']//td[contains(@class, 'processed') and contains(., 'No')]";
-
-    if (!$this->getSession()->getPage()->find("xpath", $xpath)) {
-      $this->fail("The file {$this->mediaObjects[1]->getName()} is processed before the related operation triggered.");
+      if (!$this->getSession()->getPage()->find("xpath", $xpath)) {
+        $this->fail("The file {$media_object->getName()} is processed before the related operation triggered.");
+      }
     }
   }
 
@@ -147,30 +151,79 @@ class ManageUploadedFilesViewTest extends BrowserTestBase {
    */
   public function testViewFiles() {
     $this->commonFlow();
-    $this->sendSingleFileToProcess(0, 'Yes');
-    $this->sendSingleFileToProcess(1, 'No');
+
+    $this
+      ->sendSingleFileToProcess('dummy-xsl-file.xlsx')
+      ->checkProcessResults('dummy-xsl-file.xlsx');
+
+    $this
+      ->sendSingleFileToProcess('dummy-xsl-file-2.xlsx')
+      ->checkProcessResults('dummy-xsl-file-2.xlsx');
   }
 
   /**
    * Helper function to send file for processing and validations.
    *
-   * @param $file_delta
-   *   The delta of the file from the media objects.
-   * @param $after_process_text
-   *   The text in the process field need to show.
+   * @param $file_name
+   *   The matching file name from the file lists.
+   *
+   * @return ManageUploadedFilesViewTest
+   *   The current instance.
    */
-  final protected function sendSingleFileToProcess($file_delta, $after_process_text) {
-    $media_object = $this->mediaObjects[$file_delta];
+  final protected function sendSingleFileToProcess($file_name) {
+    $media_object = $this->mediaObjects[$this->files[$file_name]['media_object_key']];
 
     // Send the first file to process.
     $xpath = "//tr[@class='media-{$media_object->id()}']//a[.='Send to process']";
     $this->getSession()->getPage()->find("xpath", $xpath)->click();
+    return $this;
+  }
+
+  /**
+   * Checking the process results.
+   *
+   * @param $file_name
+   *   The matching file name from the file lists.
+   */
+  protected function checkProcessResults($file_name) {
+    $file_metadata = $this->files[$file_name];
+
+    $media_object = $this->mediaObjects[$file_metadata['media_object_key']];
+    $after_process_text = $file_metadata['after_process_text'];
+    $process_logs = $file_metadata['logs'];
 
     // Check the file has been processed.
-    $xpath = "//tr[@class='media-{$media_object->id()}']//td[contains(@class, 'processed') and contains(., '{$after_process_text}')]";
+    $xpath = "//tr[@class='media-{$media_object->id()}']//td[contains(@class, 'processed') and contains(., '{$file_metadata['after_process_text']}')]";
+
     if (!$this->getSession()->getPage()->find("xpath", $xpath)) {
       $this->fail("The processed status for {$media_object->getName()} need to be {$after_process_text} but it's not");
     }
+
+    // Checking the process logs texts.
+    foreach ($process_logs as $process_log) {
+      $xpath =  "//tr[@class='media-{$media_object->id()}']" .
+        "//td[contains(@class, 'views-field-field-history') and contains(., '{$process_log}')]";
+
+      if (!$this->getSession()->getPage()->find("xpath", $xpath)) {
+        $this->fail("The processed log, {$process_log}, for {$media_object->getName()} does not exists");
+      }
+    }
   }
 
+  /**
+   * Testing the action for sending the files to process on the files.
+   */
+  public function _testActionForMultipleFiles() {
+    // Start with the normal stuff.
+    $this->commonFlow();
+
+    // Checking the check boxes.
+
+    // Selecting the "send files for processing" option from the list.
+
+    // Clicking on the submit button.
+
+    // Make sure we get the same results as the previous test.
+
+  }
 }
