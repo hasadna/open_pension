1f6ebffe1b6f8abe50e0756f2ba0fab89f633a88
RoySegall <roy.segall@dreamed-diabetes.com>
2019-08-19 19:53:48 +0300

    Go over the code

--- processor/app.py
+++ processor/app.py
@@ -1,53 +1,15 @@
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Resource, Api
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from logger import Logger
+
 import mongo_adapter
 import config
+from resources import QuarterReport, InvestmentHouseList, Reports
 
 app = Flask(__name__)
 api = Api(app)
 json = FlaskJSON(app)
-
-
-class QuarterReport(Resource):
-    def __init__(self, mongo):
-        self._mongo = mongo
-
-    def get(self):
-        report_databases = []
-        for db_name in self._mongo.database_names:
-            if str(db_name).startswith("report"):
-                report_databases.append(db_name)
-        return json_response(data=report_databases)
-
-
-class InvestmentHouseList(Resource):
-    def __init__(self, mongo):
-        self._mongo = mongo
-
-    def get(self):
-        report = request.args.get('quarter')
-        if not report:
-            return json_response(status_=400, message="quarter argument missing")
-        return json_response(data=self._mongo.get_collection_names(database_name=report))
-
-
-class Reports(Resource):
-    def __init__(self, mongo):
-        self._mongo = mongo
-
-    def get(self):
-        quarter = request.args.get('quarter')
-        if not quarter:
-            return json_response(status_=400, message="quarter argument missing")
-
-        investment_house = request.args.get('investment_house')
-        if not investment_house:
-            return json_response(status_=400, message="investment_house argument missing")
-
-        reports = self._mongo.get_documents(db_name=quarter, collection_name=investment_house)
-        return json_response(data=list(reports))
 
 
 if __name__ == '__main__':
@@ -59,9 +21,7 @@ if __name__ == '__main__':
                                        logger=logger)
 
     api.add_resource(QuarterReport, '/GetQuarterList', resource_class_kwargs={"mongo": mongo})
-
     api.add_resource(InvestmentHouseList, '/GetInvestmentHouseList', resource_class_kwargs={"mongo": mongo})
-
     api.add_resource(Reports, '/GetReports', resource_class_kwargs={"mongo": mongo})
 
     app.run(host="0.0.0.0", port=5000, debug=False)
--- processor/parser_report.py
+++ processor/parser_report.py
@@ -27,16 +27,19 @@ class ExcelParser:
     ISRAEL_WORDS = ['ישראל', 'בארץ']
     FIRST_FIELD_TABLE = ['שם נ"ע', 'שם המנפיק/שם נייר ערך']
     MAX_METADATA_ROWS = 10
+    SHEETS_TO_SKIP = ['סכום נכסי הקרן']
 
     def __init__(self, logger):
         self._logger = logger
         self._is_israel = None
+        self._workbook = None
 
     def parse_file(self, file_path) -> Tuple[str, dict]:
         """
-        Get pension report excel file and parse data by sheet
-        Move over all excel data sheet and parse
-        :param file_path: full file path
+        Get pension report excel file and parse data by sheet. Move over all excel data sheet and parse.
+
+        :param file_path: Full file path.
+
         :return: parsed data :type: dictionary
             """
         # Load in the workbook file
@@ -54,15 +57,17 @@ class ExcelParser:
 
         # Move over the all sheets
         for sheet_name in self._workbook.sheet_names:
-            if sheet_name == 'סכום נכסי הקרן':
+
+            if sheet_name in self.SHEETS_TO_SKIP:
+                # We need to skip this sheet.
                 continue
-            # Parse sheet
 
             try:
                 sheet_data = self._parse_sheet(sheet_name=sheet_name, orig_file=file_path)
             except Exception as e:
                 self._logger.error(f'Failed to parse {sheet_name} in {file_path}')
                 raise ExcelSheetParsingError(parse_error=str(e), sheet_name=sheet_name)
+
             if not sheet_data:
                 self._logger.warn(f'No sheet data for "{sheet_name}". maybe its empty...')
                 continue
@@ -71,32 +76,43 @@ class ExcelParser:
 
     def test_parse_file(self, file_path: str) -> Dict[str, Dict[str, list]]:
         """
-        This function is inteded to try and find all the current parsing problems
-        :param file_path: the file which we want to try and parse
-        :return: the results of the test -> dict(successful={...}, error={...})
+        This function is designed to try and find all the current parsing problems.
+
+        :param file_path: The file which we want to try and parse.
+
+        :return: The results of the test -> dict(successful={...}, error={...})
         """
         result = dict(successful=dict(), error=dict())
         processed_sheets_generator = self.parse_file(file_path=file_path)
+
         while True:
             try:
                 sheet_name, processed_sheet = next(processed_sheets_generator)
+
                 if not result['successful'].get(file_path):
                     result['successful'][file_path] = dict()
+
                 if not result['successful'][file_path].get(sheet_name):
                     result['successful'][file_path][sheet_name] = processed_sheet
+
                 result['successful'][file_path][sheet_name] = processed_sheet
+
             except StopIteration:
                 break
+
             except ExcelSheetParsingError as e:
                 self._logger.error(f'Failed to parse sheet "{e.sheet_name}": {e.parse_error}')
+
                 if not result['error'].get(e.parse_error):
                     result['error'][e.parse_error] = list()
-                result['error'][e.parse_error].append(dict(sheet_name=e.sheet_name,
-                                                           file_name=file_path))
+
+                result['error'][e.parse_error].append(dict(sheet_name=e.sheet_name, file_name=file_path))
+
             except ExcelWorkbookParsingError as e:
-                # TODO this is not working - only the last error is overriden
+                # TODO this is not working - only the last error is overridden.
                 if not result['error'].get(e.parse_error):
                     result['error'][e.parse_error] = list()
+
                 result['error'][e.parse_error].append(file_path)
 
             except Exception as e:
@@ -106,10 +122,12 @@ class ExcelParser:
 
     def _parse_sheet(self, sheet_name, orig_file="", start_row=0, start_column=2):
         """
-        Parse excel pension report sheet
-        :param sheet_name: sheet name
-        :param start_row: row number to start
-        :param start_column: column numbrt to start
+        Parse excel pension report sheet.
+
+        :param sheet_name: Sheet name
+        :param start_row: Row number to start
+        :param start_column: Column number to start
+
         :return: True / False
         """
         current_row = start_row
@@ -120,79 +138,83 @@ class ExcelParser:
         data = []
         sheet_metadata = {
             "Investment": sheet_name,
-            "orig_file" : orig_file
+            "orig_file": orig_file
         }
 
-        # Parse metadata, stop when find the first table field or until max metadata
+        # Parse metadata, stop when find the first table field or until max metadata.
         start_metadata_row = current_row
         while current_cell not in self.FIRST_FIELD_TABLE:
             if current_cell:
                 metadata = self._get_metadata(data=row_data)
+
                 if metadata:
                     translated_metadata = translate_from_hebrew(word=metadata[0])
+
                     if translated_metadata:
                         metadata_field = translated_metadata
                     else:
                         metadata_field = 'item_{0}'.format(metadata[0])
+
                     sheet_metadata[metadata_field] = metadata[1]
 
             current_row += 1
-            row_data = self._workbook.get_entire_row(sheet_name=sheet_name,
-                                                     row=current_row,
-                                                     min_column=current_column)
+            row_data = self._workbook.get_entire_row(sheet_name=sheet_name, row=current_row, min_column=current_column)
 
             if row_data:
-                # strip all spaces from start and end string
+                # Strip all spaces from start and end string.
                 current_cell = row_data[0].strip()
             else:
                 current_cell = None
 
             if current_row - start_metadata_row > self.MAX_METADATA_ROWS:
-                self._logger.error("Failed to parser sheet. max metadata rows in {0}/{1}".format(orig_file, sheet_name))
+                self._logger.error(f"Failed to parser sheet. max metadata rows in {orig_file}/{sheet_name}")
                 return None
         else:
-            # Get fields name
-            fields_name_hebrew = self._workbook.get_entire_row(sheet_name=sheet_name,
-                                                        row=current_row,
-                                                        min_column=start_column)
+            # Get fields name.
+            fields_name_hebrew = self._workbook.get_entire_row(
+                sheet_name=sheet_name,
+                row=current_row,
+                min_column=start_column
+            )
 
             fields_len = len(fields_name_hebrew)
 
             fields_name = []
             for field in fields_name_hebrew:
                 translated = translate_from_hebrew(word=str(field).strip().replace("*", ""))
+
                 if not translated:
-                    # If failed to translate append the hebrew name
-                    self._logger.warn("Failed to translate {0} from hebrew".format(field))
-                    fields_name.append("item_{0}".format(field))
+                    # If failed to translate append the hebrew name.
+                    self._logger.warn(f"Failed to translate {field} from hebrew")
+                    fields_name.append(f"item_{field}")
                 else:
                     fields_name.append(translated)
 
         empty_len = 0
         current_cell = ""
 
-        # Parsing until find the end of excel sheet
-        while current_cell not in ['* בעל ענין/צד קשור','בהתאם לשיטה שיושמה בדוח הכספי **']:
+        # Parsing until find the end of excel sheet.
+        while current_cell not in ['* בעל ענין/צד קשור', 'בהתאם לשיטה שיושמה בדוח הכספי **']:
 
             if empty_len > 5:
-                # self._logger.info("max empty row")
                 break
 
-            # Get next row
             current_row += 1
-            data_row = self._workbook.get_entire_row(sheet_name=sheet_name,
-                                                     row=current_row,
-                                                     min_column=start_column,
-                                                     max_column=fields_len+start_column)
+            data_row = self._workbook.get_entire_row(
+                sheet_name=sheet_name,
+                row=current_row,
+                min_column=start_column,
+                max_column=fields_len + start_column
+            )
 
-            # Check if is empty row or first cell is empty
+            # Check if is empty row or first cell is empty.
             if not data_row or not data_row[0]:
                 empty_len += 1
                 continue
 
             current_cell = data_row[0]
 
-            # if current cell start is total row
+            # if current cell start is total row.
             if current_cell.find('סה"כ') != -1:
                 self._parse_total_field(current_cell)
                 continue
@@ -203,10 +225,6 @@ class ExcelParser:
                     "Line in file": current_row
                 }
 
-                # if another row is empty continue
-                # if not all(data_row[1:]):
-                #     continue
-
                 for i in range(0, fields_len):
                     try:
                         row[fields_name[i].strip()] = data_row[i]
@@ -214,40 +232,43 @@ class ExcelParser:
                         self._logger.error("Failed {0} {1}".format(ex, fields_name))
 
                 if "Instrument Number" in row and row["Instrument Number"]:
-                    # Add metadata
+                    # Add metadata and add row data to data list.
                     row.update(sheet_metadata)
-                    # Add row data to data list
                     data.append(row)
 
         return data
 
     def _get_metadata(self, data):
         """
-        Parse metadata data
+        Parse metadata data.
+
         :param data: list of data   
+
         :return:
         """
         first_cell = data[0]
+
         if not first_cell:
             self._logger.error("No data in first cell")
             return None, None
 
         finder = first_cell.find(":")
-        # find func return -1 when not find
+        # Find func return -1 when not find.
         if finder != -1:
-            # Check if the colon char is not last data char (The meaning data in the first cell)
+            # Check if the colon char is not last data char (The meaning data in the first cell).
             if len(first_cell) > finder:
-                return first_cell[:finder], first_cell[finder+1:]
+                return first_cell[:finder], first_cell[finder + 1:]
 
-        # check if len of data is bigger than one
+        # Check if len of data is bigger than one.
         elif len(data) > 1:
             return first_cell, data[1]
 
     def _parse_total_field(self, data):
         """
-        Parse total field, total filed start with 'סה"כ' word
-        In total field we get if the investment in Israel
+        Parse total field, total filed start with 'סה"כ' word in total field we get if the investment in Israel.
+
         :param data:
+
         :return:
         """
         # strip 'סה"כ' word
@@ -263,23 +284,30 @@ class ExcelParser:
 
         if recursive_finder(words_list=self.ISRAEL_WORDS):
             self._is_israel = True
-            # print("{0} is israel".format(data))
         elif recursive_finder(words_list=self.NOT_ISRAEL_WORDS):
             self._is_israel = False
-            # print("{0} is not israel".format(data))
-
-        # print(self._total_data)
 
 
 def save_to_json_file(path, file_name, data):
+    """
+    Saving json to file. Probably.
+
+    :param path: The path of the file.
+    :param file_name: The file name.
+    :param data: The data to write.
+    """
     if not os.path.isdir(path):
-        raise Exception("folder not exists {0}".format(path))
+        raise Exception(f"folder not exists {path}")
 
     full_path = os.path.join(path, file_name)
+
     try:
+
         with open(full_path, "w") as outfile:
             json.dump(data, outfile)
+
         return True
+
     except Exception as ex:
         raise ValueError("Failed to write json file {0}".format(ex))
 
@@ -290,11 +318,14 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
+
     parser.add_argument('--root', type=str, help='The excels root paths')
     parser.add_argument('--investment_house', type=str, help='the related investment house')
     parser.add_argument('--test', '-t', action='store_true')
     parser.add_argument('--test_file', type=str)
+
     args = parser.parse_args()
+
     root_path = args.root
     test_parsing = args.test
     test_file = args.test_file
@@ -322,40 +353,44 @@ if __name__ == '__main__':
 
     for root, dirs, files in os.walk(root_path, followlinks=False):
 
-            logger.info(msg="Start working on {0} investment house: {1}".format(file_path, investment_house))
-            for sheet_name, sheet_data in process_xl.parse_file(file_path=file_path):
-                if not sheet_data:
-                    logger.warn("Not got data from sheet")
+        logger.info(msg=f"Start working on {file_path} investment house: {investment_house}")
+        for sheet_name, sheet_data in process_xl.parse_file(file_path=file_path):
+
+            if not sheet_data:
+                logger.warn("Not got data from sheet")
+                continue
+
+            c = 0
+            for data in sheet_data:
+
+                if 'מספר ני"ע' in data and not data['מספר ני"ע']:
+                    c += 1
                     continue
 
-                c = 0
+                if not mongo.insert_document(db_name=db_name, collection_name=investment_house, data=data):
+                    print("Failed to insert document to mongodb")
+
+        logger.info(f"Done with {file}")
+        logger.info(msg=f'Start working on {file_path} investment house: {investment_house}')
+
+        if test_parsing:
+            result = excel_parser.test_parse_file(file_path=file_path)
+
+            with open(test_file, 'w+') as test_result:
+                test_result.write(json.dumps(result, indent=4))
+        else:
+            for sheet_name, sheet_data in excel_parser.parse_file(file_path=file_path):
                 for data in sheet_data:
+
                     if 'מספר ני"ע' in data and not data['מספר ני"ע']:
-                        c += 1
                         continue
-                    if not mongo.insert_document(db_name=db_name,
-                                                 collection_name=investment_house,
-                                                 data=data):
-                        print("Failed to insert document to mongodb")
 
-            logger.info("Done with {0}".format(file))
-            logger.info(msg=f'Start working on {file_path} investment house: {investment_house}')
+                    # TODO aggregate insert operations for later bulk insert
+                    # if not mongo.insert_document(db_name=DB_NAME,
+                    #                              collection_name=investment_house,
+                    #                              data=data):
+                    #     print("Failed to insert document to mongodb")
 
-            if test_parsing:
-                result = excel_parser.test_parse_file(file_path=file_path)
-                with open(test_file, 'w+') as test_result:
-                    test_result.write(json.dumps(result, indent=4))
-            else:
-                for sheet_name, sheet_data in excel_parser.parse_file(file_path=file_path):
-                    for data in sheet_data:
-                        if 'מספר ני"ע' in data and not data['מספר ני"ע']:
-                            continue
-                        # TODO aggregate insert operations for later bulk insert
-                        # if not mongo.insert_document(db_name=DB_NAME,
-                        #                              collection_name=investment_house,
-                        #                              data=data):
-                        #     print("Failed to insert document to mongodb")
+        # TODO bulk insert into MongoDB
 
-            # TODO bulk insert into MongoDB
-
-            logger.info(f'Done with {file}')
+        logger.info(f'Done with {file}')
