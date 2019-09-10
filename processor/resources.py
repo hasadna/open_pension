16ea9e24fa1885847e089dcd3e7e10fe7de07c6d
RoySegall <roy.segall@dreamed-diabetes.com>
2019-09-10 08:22:15 +0300

    Commit files

--- processor/resources.py
+++ processor/resources.py
@@ -1,6 +1,8 @@
 from flask import request
 from flask_restful import Resource
 from flask_json import json_response
+from flask_restful import reqparse
+import werkzeug
 
 
 class UploadFile(Resource):
@@ -9,6 +11,17 @@ class UploadFile(Resource):
         """
         Uploading a file to the system for processing.
         """
+        parser = reqparse.RequestParser()
+        parser.add_argument(
+            'file',
+            type=werkzeug.datastructures.FileStorage,
+            help='File to store and process later on',
+            location='files'
+        )
+
+        args = parser.parse_args()
+        print(args)
+
         return json_response(data={'message': 'Uploading files'})
 
 
