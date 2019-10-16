d82df2fdfb5e4d522803e07e274692bb60a2335f
Roy Segall <roy@segall.io>
2019-10-16 07:35:20 +0300

    More work on pushing to atlas.

--- processor/push_to_atlas.py
+++ processor/push_to_atlas.py
@@ -1,7 +1,24 @@
 from mongodb import Mongo
+import os
+import json
+
 
 mongo = Mongo()
 mongo.set_client("mongodb+srv://admin:sam@processedfiles-aupgc.mongodb.net/test?retryWrites=true&w=majority")
 
+print("Remove all records form the DB")
 mongo.delete_all()
-mongo.insert_multiple([{"item": "one"}, {"item": "second"}])
+
+print("Start to collect the records")
+items = []
+directory = os.path.join(os.getcwd(), "fdb-output")
+
+for filename in os.listdir(directory):
+    if filename.endswith(".json"):
+        with open(os.path.join(directory, filename)) as file:
+            items.append(json.load(file))
+            break
+
+print(items)
+print("Push to mongo")
+mongo.insert_multiple(items)
