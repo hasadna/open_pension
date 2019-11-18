420dcf799a39752005120eb2adae5ecabf1600d9
amitsrour <amitsrour@gmail.com>
2019-11-18 20:44:24 +0200

    Trying to fix travis

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Dev..</p>
+        <p>Dev...</p>
         <a
           className="App-link"
           href="https://reactjs.org"
