37d99bcf0025db2af27626fb9faebab5934a21cd
ItamarGronich <itamargronich@gmail.com>
2019-12-16 21:17:50 +0200

    fix docker image.

--- cms/entrypoint.sh
+++ cms/entrypoint.sh
@@ -1,3 +1,22 @@
 #! /bin/bash
-echo "Starting Zookeeper..."
-echo "Starting kafka server..."
+
+# Check if DB exists.
+echo "Waiting for DB connection"
+while ! echo exit | nc $MYSQL_HOST $MYSQL_PORT;
+do
+    echo "DB still off-line....";
+    sleep 10;
+done
+
+# Install drupal if not already installed
+../vendor/drupal site:install open_pension mysql://user:password@cms_db/db \
+  --langcode="en" \
+  --site-name="Drupal 8" \
+  --site-mail="admin@example.com" \
+  --account-name="admin" \
+  --account-mail="admin@example.com" \
+  --account-pass="admin" \
+  --no-interaction
+
+## Install modules which cannot be installed as dependencies of the profile.
+../vendor/drush en open_pension_blog open_pension_files open_pension_logs
