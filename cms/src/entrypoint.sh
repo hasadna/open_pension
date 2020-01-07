58c7740f2420657dbcbed43f250052ea0e9b260d
ItamarGronich <itamargronich@gmail.com>
2020-01-07 09:04:36 +0200

    Make drupal work.

--- cms/src/entrypoint.sh
+++ cms/src/entrypoint.sh
@@ -2,11 +2,14 @@
 
 # Check if DB exists.
 echo "Waiting for DB connection"
-while ! echo exit | nc $MYSQL_HOST $MYSQL_PORT;
+while ! echo exit | nc -vz $MYSQL_HOST $MYSQL_PORT;
 do
     echo "DB still off-line....";
     sleep 10;
 done
+echo "DB is online"
 
 # Install drupal if not already installed
 vendor/bin/drush si open_pension --db-url=mysql://user:password@cms_db/db --account-pass="admin" --account-name="admin" -y -v
+
+exec apache2-foreground
