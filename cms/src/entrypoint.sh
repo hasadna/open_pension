c2c4275d8c835c6993753eba0c839ab692c676ef
Roy Segall <roy@segall.io>
2020-01-14 18:32:43 +0200

    Adding the env files.

--- cms/src/entrypoint.sh
+++ cms/src/entrypoint.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-echo "asasdasd"
-
-echo $MYSQL_HOST
-
 # Check if DB exists.
 echo "Waiting for DB connection"
 while ! echo exit | nc -vz $MYSQL_HOST $MYSQL_PORT;
@@ -16,6 +12,10 @@ echo "DB is online"
 # Install drupal if not already installed
 # Create a drush commnand for checking if drupal installed or not - if so just run updb.
 vendor/bin/drush si open_pension --db-url="mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE}" --account-pass="${ACCOUNT_PASS}" --account-name="${ACCOUNT_NAME}" -y -v
+
+# Clear cache for css and js issues.
+chmod 777 -R web/sites/default/files/
+vendor/bin/drush cr
 
 # Start the apache process.
 exec apache2-foreground
