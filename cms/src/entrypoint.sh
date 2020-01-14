c392a8e32ad92ef715de1998cf5557f741cc968b
RoySegall <roy.segall@dreamed-diabetes.com>
2020-01-14 14:56:39 +0200

    Commit progress

--- cms/src/entrypoint.sh
+++ cms/src/entrypoint.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+echo "asasdasd"
+
+echo $MYSQL_HOST
+
 # Check if DB exists.
 echo "Waiting for DB connection"
 while ! echo exit | nc -vz $MYSQL_HOST $MYSQL_PORT;
@@ -10,7 +14,8 @@ done
 echo "DB is online"
 
 # Install drupal if not already installed
-vendor/bin/drush si open_pension --db-url=mysql://user:password@cms_db/db --account-pass="admin" --account-name="admin" -y -v
+# Create a drush commnand for checking if drupal installed or not - if so just run updb.
+vendor/bin/drush si open_pension --db-url="mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE}" --account-pass="${ACCOUNT_PASS}" --account-name="${ACCOUNT_NAME}" -y -v
 
 # Start the apache process.
 exec apache2-foreground
