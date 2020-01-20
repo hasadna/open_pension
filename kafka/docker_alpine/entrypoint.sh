c8a3a17cda37781f6e6764ad3e38963b1ebe977c
Ilan <44946919+ilansh7@users.noreply.github.com>
2020-01-20 18:39:36 +0200

    Delete entrypoint.sh

--- kafka/docker_alpine/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-echo "Rendering Kafka Config..."
-envsubst < server.properties.template > config/server.properties
-
-echo "Starting kafka server..."
-bin/kafka-server-start.sh config/server.properties
