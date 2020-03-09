3e79793c5e4b86fd2b863a4609eae181d6d3562c
RoySegall <roy@segall.io>
2020-03-09 06:42:58 +0200

    Get the label of the values. Now, let's get the proper value.

--- processor/src/performance.py
+++ processor/src/performance.py
@@ -6,29 +6,17 @@ p = re.compile('[0-9]{4}')
 anchor_cell_value = 'מזומנים ושווי מזומנים'
 months = {
     1: 'ינואר',
-    2: 'ינואר',
-    3: 'פברואר',
-    4: 'פברואר',
-    5: 'מרץ',
-    6: 'מרץ',
-    7: 'אפריל',
-    8: 'אפריל',
-    9: 'מאי',
-    10: 'מאי',
-    11: 'יוני',
-    12: 'יוני',
-    13: 'יולי',
-    14: 'יולי',
-    15: 'אוגוסט',
-    16: 'אוגוסט',
-    17: 'ספטמבר',
-    18: 'ספטמבר',
-    19: 'נובמבר',
-    20: 'נובמבר',
-    21: 'אוקטובר',
-    22: 'אוקטובר',
-    23: 'דצמבר',
-    24: 'דצמבר',
+    2: 'פברואר',
+    3: 'מרץ',
+    4: 'אפריל',
+    5: 'מאי',
+    6: 'יוני',
+    7: 'יולי',
+    8: 'אוגוסט',
+    9: 'ספטמבר',
+    10: 'נובמבר',
+    11: 'אוקטובר',
+    12: 'דצמבר',
 }
 
 
@@ -88,12 +76,30 @@ def collect_table(worksheet: Worksheet, row, column) -> object:
     for iterated_row in range(row, row + 21):
         row_label: str = worksheet.cell(row=iterated_row, column=column).value
 
-        row_data = []
+        row_data = {}
         for iterated_column in range(column + 1, column + 1 + 24):
+
+            # Getting the matching month of the current cell and resetting
+            # the cell value.
+            month_index = int(iterated_column / 2)
+            month_name = months[month_index]
+
+            if month_name not in row_data:
+                row_data[month_name] = {
+                    'התרומה לתשואה': '',
+                    'שיעור מסך הנכסים': '',
+                }
 
             cell_value = worksheet.cell\
                 (row=iterated_row, column=iterated_column).value
-            row_data.append(cell_value)
+
+            # Values in cell with even index number have a label and odd
+            # indexed-cell have a different label.
+            if iterated_column % 2 == 0:
+                key = 'התרומה לתשואה'
+            else:
+                key = 'שיעור מסך הנכסים'
+            row_data[month_name][key] = cell_value
 
         data[row_label.strip()] = row_data
 
