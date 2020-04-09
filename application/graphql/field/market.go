caaae4d4bd151aefde055f7831970ee4104d4c3f
RoySegall <roy@segall.io>
2020-04-09 18:41:20 +0300

    Doen with migrating data to the structure

--- application/graphql/field/market.go
+++ application/graphql/field/market.go
@@ -10,7 +10,13 @@ var market = graphql.NewObject(
 	graphql.ObjectConfig{
 		Name: "Market",
 		Fields: graphql.Fields{
-			"id":                   &graphql.Field{Type: graphql.ID},
+			"id":          &graphql.Field{Type: graphql.ID},
+			"market_name": &graphql.Field{Type: graphql.String},
+			"market_code": &graphql.Field{Type: graphql.String},
+			"country":     &graphql.Field{Type: graphql.ID},
+			"created_at":  &graphql.Field{Type: graphql.DateTime},
+			"updated_at":  &graphql.Field{Type: graphql.DateTime},
+			"deleted_at":  &graphql.Field{Type: graphql.DateTime},
 		},
 		Description: "Market fields",
 	},
@@ -37,8 +43,8 @@ func Market(db *gorm.DB) *graphql.Field {
 		Type: market,
 		Args: graphql.FieldConfigArgument{
 			"id": &graphql.ArgumentConfig{
-				Type: graphql.Int,
-				Description: "Filter market by a given ID",
+				Type:         graphql.Int,
+				Description:  "Filter market by a given ID",
 				DefaultValue: 0,
 			},
 		},
--- application/sandbox.go
+++ application/sandbox.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/hasadna/open_pension/application/Models"
 	"github.com/hasadna/open_pension/application/api"
 )
@@ -23,22 +20,6 @@ func main() {
 		Models.Market{},
 	)
 
-	var company Models.Company
-
-	db.Preload("Country").Find(&company)
-
-	//fmt.Print(company)
-
-	b, err := json.Marshal(company)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(string(b))
-
-	//db.Model(&Models.Company{}).AddForeignKey("country_id", "country(id)", "CASCADE", "CASCADE")
-	//
-	//country := Models.Country{Name: "a"}
-	//db.Save(&country)
-	//db.Save(&Models.Company{CompanyName: "a", CountryId: country.ID})
+	// Setting up foreign keys.
+	db.Model(&Models.Company{}).AddForeignKey("country_id", "country(id)", "CASCADE", "CASCADE")
 }
