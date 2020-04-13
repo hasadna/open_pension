26365ad5308ad15078f055d277b5aee5284cd6e4
RoySegall <roy@segall.io>
2020-04-13 20:15:54 +0300

    More work on the other stuff

--- application/graphql/mutation/processing.go
+++ application/graphql/mutation/processing.go
@@ -24,32 +24,32 @@ func JsonStringParse(JsonString string) (Payload, error) {
 
 func MigrateProcessedObject(parsedPayload Payload, db *gorm.DB) Result {
 	processSheet(parsedPayload.Caches, db)
-	//processSheet(parsedPayload.GovernmentBond)
-	//processSheet(parsedPayload.CommercialDebt)
-	//processSheet(parsedPayload.CorporateBond)
-	//processSheet(parsedPayload.Stock)
-	//processSheet(parsedPayload.ETF)
-	//processSheet(parsedPayload.MutualFund)
-	//processSheet(parsedPayload.Warrant)
-	//processSheet(parsedPayload.Structured)
-	//processSheet(parsedPayload.NotTradedGovernmentBond)
-	//processSheet(parsedPayload.NotTradedCommercialDebt)
-	//processSheet(parsedPayload.NotTradedCorporateBond)
-	//processSheet(parsedPayload.NotTradedStock)
-	//processSheet(parsedPayload.NotTradedPrivateEquity)
-	//processSheet(parsedPayload.NotTradedWarrant)
-	//processSheet(parsedPayload.NotTradedOption)
-	//processSheet(parsedPayload.NotTradedFuture)
-	//processSheet(parsedPayload.NotTradedStructured)
-	//processSheet(parsedPayload.NotTradedLoans)
-	//processSheet(parsedPayload.Deposits)
-	//processSheet(parsedPayload.RealEstate)
-	//processSheet(parsedPayload.PortfolioCompanies)
-	//processSheet(parsedPayload.OtherInvestments)
-	//processSheet(parsedPayload.InvestmentCommitments)
-	//processSheet(parsedPayload.FairValue)
-	//processSheet(parsedPayload.NotTradedFairValue)
-	//processSheet(parsedPayload.CreditFairValue)
+	//processSheet(parsedPayload.GovernmentBond, db)
+	//processSheet(parsedPayload.CommercialDebt, db)
+	//processSheet(parsedPayload.CorporateBond, db)
+	//processSheet(parsedPayload.Stock, db)
+	//processSheet(parsedPayload.ETF, db)
+	//processSheet(parsedPayload.MutualFund, db)
+	//processSheet(parsedPayload.Warrant, db)
+	//processSheet(parsedPayload.Structured, db)
+	//processSheet(parsedPayload.NotTradedGovernmentBond, db)
+	//processSheet(parsedPayload.NotTradedCommercialDebt, db)
+	//processSheet(parsedPayload.NotTradedCorporateBond, db)
+	//processSheet(parsedPayload.NotTradedStock, db)
+	//processSheet(parsedPayload.NotTradedPrivateEquity, db)
+	//processSheet(parsedPayload.NotTradedWarrant, db)
+	//processSheet(parsedPayload.NotTradedOption, db)
+	//processSheet(parsedPayload.NotTradedFuture, db)
+	//processSheet(parsedPayload.NotTradedStructured, db)
+	//processSheet(parsedPayload.NotTradedLoans, db)
+	//processSheet(parsedPayload.Deposits, db)
+	//processSheet(parsedPayload.RealEstate, db)
+	//processSheet(parsedPayload.PortfolioCompanies, db)
+	//processSheet(parsedPayload.OtherInvestments, db)
+	//processSheet(parsedPayload.InvestmentCommitments, db)
+	//processSheet(parsedPayload.FairValue, db)
+	//processSheet(parsedPayload.NotTradedFairValue, db)
+	//processSheet(parsedPayload.CreditFairValue, db)
 
 	result := Result{Data: "Passed", Error: ""}
 	return result
@@ -62,30 +62,30 @@ func processSheet(PayloadRecords []PayloadRecord, db *gorm.DB) {
 }
 
 func processRecord(payloadRecord PayloadRecord, db *gorm.DB) {
-
-	// todo:
-	//  4. Create Markets
-	//  5. Create Instrument
-	// 	6. Add the filename, sheet and line in file to the DB
-
 	// Create instrument data by company.
 	instrumentData := Models.InstrumentDateByCompany{}
 	instrumentData.Currency = payloadRecord.Currency
-	//instrumentData.Duration = ''
-	//instrumentData.Rate = ''
-	//instrumentData.Price = ''
-	//instrumentData.RateRegister = ''
 
+	//// מח״מ
+	//instrumentData.Duration = payloadRecord.Duration
+	//
+	//// ?
+	//instrumentData.Rate = payloadRecord.Rate
+	//
+	//// ?
+	//instrumentData.Price = payloadRecord.Price
+	//
+	//// ?
+	//instrumentData.RateRegister = payloadRecord.RateOfRegister
 
 	// Other properties which need special logic will be handled in other functions.
 	AttachFairValue(payloadRecord, &instrumentData)
 	AttachDates(payloadRecord, &instrumentData)
 
-	// todo: Does fund, instrument and company are unique for each one? If so, is that mean the when crating an
-	// 	instrument we need to attach the issuer reference (company) to the fund and the investing company?
 	AttachInstrument(payloadRecord, db, &instrumentData)
 	AttachFund(payloadRecord, db, &instrumentData)
-	AttachLooseEnds(payloadRecord, db, &instrumentData)
+
+	instrumentData.InvestingCompanyId = instrumentData.InstrumentNumber.IssuerNumberId
 
 	db.Save(&instrumentData)
 }
@@ -99,66 +99,66 @@ func AttachDates(payloadRecord PayloadRecord, instrumentData *Models.InstrumentD
 
 	// Maybe take the date?
 	parsedDate, _ := time.Parse("02/01/2006", payloadRecord.Date)
+
+	// Not always exists.
 	instrumentData.PurchaseDate = parsedDate
+
+	// Take from the header of the file.
 	instrumentData.ReportDate = parsedDate
 }
 
+// Create the fund from the metadata on the file.
 func AttachFund(payloadRecord PayloadRecord, db *gorm.DB, instrumentData *Models.InstrumentDateByCompany) {
+	company := Models.Company{}
+	db.FirstOrCreate(&company, Models.Company{CompanyName: payloadRecord.InstitutionalBody})
+
 	fund := Models.Fund{}
-	db.FirstOrCreate(&fund, Models.Fund{FundName: payloadRecord.FundName, FundNumber: payloadRecord.FundNumber})
+	db.FirstOrCreate(&fund, Models.Fund{FundName: payloadRecord.FundName, FundNumber: payloadRecord.FundNumber, ExecutiveBodyId: company.ID})
 	instrumentData.FundId = fund.ID
 }
 
 func AttachInstrument(payloadRecord PayloadRecord, db *gorm.DB, instrumentData *Models.InstrumentDateByCompany) {
-	// todo: ask if an instrument can be shared among several records.
 	instrument := Models.Instrument{}
 
-	// todo: how to get the country?
-	// todo: what to do when there's no market name? from where to get the market code?
 	market := Models.Market{}
 	db.FirstOrCreate(&market, Models.Market{MarketName: payloadRecord.MarketName})
 
 	// Creating company.
-	// todo: ask moshe - does the CompanyLocalNumber is the what exists in the table?
 	issuer := Models.Company{}
 	db.FirstOrCreate(&issuer, Models.Company{CompanyLocalNumber: payloadRecord.IssuerNumber})
 
 	// Finally, create the instrument.
 	db.FirstOrCreate(&instrument, Models.Instrument{
-		InvestmentType: payloadRecord.Investment,
-		InstrumentName: payloadRecord.InstrumentName,
+		InvestmentType:   payloadRecord.Investment,
+		InstrumentName:   payloadRecord.InstrumentName,
 		InstrumentNumber: payloadRecord.InstrumentNumber,
-		IssuerNumberId: issuer.ID,
-		Industry: payloadRecord.Industry,
-		MarketId: market.ID,
+		IssuerNumberId:   issuer.ID,
+		Industry:         payloadRecord.Industry,
+		MarketId:         market.ID,
 	})
 
+	// Reference the instrument to the object we created so we can use it later on.
 	instrument.IssuerNumber = issuer
 	instrument.Market = market
 
+	// Add the ID of the instrument on the instrument data along side the instrument object (for later reference).
 	instrumentData.InstrumentNumberId = instrument.ID
 	instrumentData.InstrumentNumber = instrument
 }
 
 func AttachFairValue(payloadRecord PayloadRecord, instrumentData *Models.InstrumentDateByCompany) {
+
+	if payloadRecord.FairValue == "" {
+		instrumentData.FairValue = 0
+		return
+	}
+
 	FairValue, err := strconv.ParseFloat(payloadRecord.FairValue, 10)
 
 	if err != nil {
+		//matched, err := regexp.Match(`foo.*`, []byte(`seafood`))
 		panic(err)
 	}
 
 	instrumentData.FairValue = FairValue
-}
-
-func AttachLooseEnds(payloadRecord PayloadRecord, db *gorm.DB, instrumentData *Models.InstrumentDateByCompany) {
-	// Connect all the entities we created during the parsing.
-	instrumentData.InvestingCompanyId = instrumentData.InstrumentNumber.IssuerNumberId
-
-	//fund := Models.Fund{}
-	//
-	//db.First(&fund, instrumentData.FundId)
-	//fmt.Println(fund.ExecutiveBodyId, fund.ExecutiveBody)
-
-	// Attach the fund to the executive body.
-	db.Model(&Models.Fund{}).Where("id = ?", instrumentData.FundId).Updates(Models.Fund{ExecutiveBodyId:instrumentData.InvestingCompanyId})
 }
