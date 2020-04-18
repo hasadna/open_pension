c65047e96449dc66ce9f1e9b40af6f8f45ee718b
Roy Segall <roy@segall.io>
2020-04-18 12:22:39 +0300

    Setting up the flow fo the field.

--- cms/src/web/profiles/open_pension/modules/open_pension_files/src/Plugin/views/field/ProcessorStatus.php
+++ cms/src/web/profiles/open_pension/modules/open_pension_files/src/Plugin/views/field/ProcessorStatus.php
@@ -12,6 +12,10 @@ use Drupal\views\ResultRow;
  */
 class ProcessorStatus extends FieldPluginBase {
 
+  const STATUS_NEW = 'New';
+  const PROCESSED = 'processed';
+  const PROCESSED_WITH_ERRORS = 'processed with errors';
+
   /**
    * {@inheritdoc}
    */
@@ -23,8 +27,30 @@ class ProcessorStatus extends FieldPluginBase {
    * {@inheritdoc}
    */
   public function render(ResultRow $values) {
-    $value = parent::render($values);
-    return "🍕";
+    /** @var \Drupal\media\Entity\Media $entity */
+    $entity = $this->getEntity($values);
+
+    // Get the process status.
+    $status = $entity->get('field_processing_status')->value;
+    $other_service_status = $entity->get('field_reference_in_other_service')->value;
+
+    $params = [
+      '@process-status' => $status,
+      '@process-id' => $other_service_status,
+      '@trigger-link' => '<b>Trigger link</b>',
+      '@download-link' => '<b>Download link</b>',
+    ];
+
+    if ($other_service_status == NULL) {
+      return t('Send to processor: @trigger-link', $params);
+    }
+
+    if ($status == self::STATUS_NEW) {
+      return t('New (@process-status) - Trigger process', $params);
+    }
+    else {
+      return t('@process-status (@process-id), @download-link', $params);
+    }
   }
 
 }
