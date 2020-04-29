fe8c99d1a7cad47f39b1dc6a5aa5961b72e35be2
RoySegall <roy@segall.io>
2020-04-29 09:49:43 +0300

    Done the report type

--- fetcher/src/services/query-services.ts
+++ fetcher/src/services/query-services.ts
@@ -1,5 +1,5 @@
 /**
- * Get the list of companies and their identifier.
+ * Returns the list of companies and their identifier.
  */
 export function getCompanies() {
     return [
@@ -259,19 +259,29 @@ export function getCompanies() {
 }
 
 /**
- * Get the list of system fields.
+ * Returns the list of system fields.
  */
 export function getSystemFields() {
     return [
         {Id: "", Label: "הכל"},
-        {Id: "300003", Label: "גמל"},
         {Id: "300001", Label: "ביטוח"},
         {Id: "300002", Label: "פנסיה"},
+        {Id: "300003", Label: "גמל"},
     ];
 }
 
+/**
+ * Returns the list of reports types.
+ */
 export function getReportsType() {
     return [
         {Id: "", Label: "הכל"},
+        {Id: "71100004", Label: "דוח תקופתי רבועני ביטוח בקובץ PDF"},
+        {Id: "71100028", Label: "דוח תקופתי רבעוני בקובץ PDF - פנסיה"},
+        {Id: "71100071", Label: "רשימת נכסים ברמת נכס בודד - Public - חברה - ביטוח"},
+        {Id: "71100074", Label: "רשימת נכסים ברמת נכס בודד - Public - חברה - פנסיה"},
+        {Id: "71100072", Label: "רשימת נכסים ברמת נכס בודד - Public - מסלול ביטוח"},
+        {Id: "71100075", Label: "רשימת נכסים ברמת נכס בודד - Public - מסלול פנסיה"},
+        {Id: "71100077", Label: "רשימת נכסים ברמת נכס בודד - Public - מוצר - פנסיה"},
     ];
 }
