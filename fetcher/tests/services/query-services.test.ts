7850603bd02734156edb7f7efb11af3f0f4cd5a8
RoySegall <roy@segall.io>
2020-05-02 17:57:21 +0300

    Adding more tests

--- fetcher/tests/services/query-services.test.ts
+++ fetcher/tests/services/query-services.test.ts
@@ -1,7 +1,37 @@
+import {getPeriodRanges, getReportsType, getSystemFields} from "services/query-services";
+
 describe("Testing the query resolvers", () => {
 
-    it('First test', () => {
-        expect(true).toBeTruthy()
+    it('Testing the system fields', () => {
+        expect(getSystemFields()).toStrictEqual([
+            {Id: "", Label: "הכל"},
+            {Id: "300001", Label: "ביטוח"},
+            {Id: "300002", Label: "פנסיה"},
+            {Id: "300003", Label: "גמל"},
+        ]);
+    })
+
+    it('Testing the report types', () => {
+        expect(getReportsType()).toStrictEqual([
+            {Id: "", Label: "הכל"},
+            {Id: "71100004", Label: "דוח תקופתי רבועני ביטוח בקובץ PDF"},
+            {Id: "71100028", Label: "דוח תקופתי רבעוני בקובץ PDF - פנסיה"},
+            {Id: "71100071", Label: "רשימת נכסים ברמת נכס בודד - Public - חברה - ביטוח"},
+            {Id: "71100074", Label: "רשימת נכסים ברמת נכס בודד - Public - חברה - פנסיה"},
+            {Id: "71100072", Label: "רשימת נכסים ברמת נכס בודד - Public - מסלול ביטוח"},
+            {Id: "71100075", Label: "רשימת נכסים ברמת נכס בודד - Public - מסלול פנסיה"},
+            {Id: "71100077", Label: "רשימת נכסים ברמת נכס בודד - Public - מוצר - פנסיה"},
+        ]);
+    })
+
+    it('Testing the dates period', () => {
+        expect(getPeriodRanges()).toStrictEqual({
+            "Quarters": [{"Id": "1", "Label": "רבעון ראשון"}, {
+                "Id": "2",
+                "Label": "רבעון שני"
+            }, {"Id": "3", "Label": "רבעון שלישי"}, {"Id": "4", "Label": "שנתי"}],
+            "Years": [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
+        });
     })
 
 });
