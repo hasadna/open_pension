c8a96c6e03236f2c1a4656ad99f3bf4b2e036688
Roy Segall <roy@segall.io>
2020-05-21 21:05:31 +0300

    Move the application to Go and a MySQL

---
+++ application/graphql/field/instrumentDateByCompany.go
@@ -0,0 +1,103 @@
+package field
+
+import (
+	"github.com/graphql-go/graphql"
+	"github.com/hasadna/open_pension/application/Models"
+	"github.com/jinzhu/gorm"
+)
+
+var instrumentDateByCompany = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "InstrumentDateByCompany",
+		Fields: graphql.Fields{
+			"id":                           &graphql.Field{Type: graphql.ID},
+			"currency":                     &graphql.Field{Type: graphql.String},
+			"duration":                     &graphql.Field{Type: graphql.Float},
+			"fair_value":                   &graphql.Field{Type: graphql.Int},
+			"nominal_value":                &graphql.Field{Type: graphql.Float},
+			"price":                        &graphql.Field{Type: graphql.Float},
+			"purchase_date":                &graphql.Field{Type: graphql.DateTime},
+			"rate":                         &graphql.Field{Type: graphql.Float},
+			"fund_rate_holding":            &graphql.Field{Type: graphql.Float},
+			"rate_funding_instrument_type": &graphql.Field{Type: graphql.Float},
+			"rate_register":                &graphql.Field{Type: graphql.Float},
+			"report_date":                  &graphql.Field{Type: graphql.DateTime},
+			"created_at":                   &graphql.Field{Type: graphql.DateTime},
+			"updated_at":                   &graphql.Field{Type: graphql.DateTime},
+			"deleted_at":                   &graphql.Field{Type: graphql.DateTime},
+
+			"invest_company": &graphql.Field{
+				Type: company,
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					return p.Source.(Models.InstrumentDateByCompany).InvestingCompany, nil
+				},
+			},
+			"instrument_number": &graphql.Field{
+				Type: instrument,
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					return p.Source.(Models.InstrumentDateByCompany).InstrumentNumber, nil
+				},
+			},
+			"fund_id": &graphql.Field{
+				Type: fund,
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					return p.Source.(Models.InstrumentDateByCompany).Fund, nil
+				},
+			},
+		},
+		Description: "Instrument date by company fields",
+	},
+)
+
+func InstrumentDateByCompanies(db *gorm.DB) *graphql.Field {
+	return &graphql.Field{
+		Type: graphql.NewList(instrumentDateByCompany),
+		Resolve: func(p graphql.ResolveParams) (i interface{}, e error) {
+			var instrumentDateByCompanies []Models.InstrumentDateByCompany
+
+			if err := db.
+				Preload("Fund").
+				Preload("InstrumentNumber").
+				Preload("InstrumentNumber.IssuerNumber").
+				Preload("InvestingCompany").
+				Preload("InvestingCompany.Country").
+				Find(&instrumentDateByCompanies).
+				Error; err != nil {
+				panic(err)
+			}
+
+			return instrumentDateByCompanies, nil
+		},
+		Description: "A list of instrument date by companies",
+	}
+}
+
+func InstrumentDateByCompany(db *gorm.DB) *graphql.Field {
+	return &graphql.Field{
+		Type: instrumentDateByCompany,
+		Args: graphql.FieldConfigArgument{
+			"id": &graphql.ArgumentConfig{
+				Type:         graphql.Int,
+				Description:  "Filter instrument date by company by a given ID",
+				DefaultValue: 0,
+			},
+		},
+		Resolve: func(p graphql.ResolveParams) (i interface{}, e error) {
+			instrumentDateByCompany := Models.InstrumentDateByCompany{}
+
+			if err := db.
+				Preload("Fund").
+				Preload("InstrumentNumber").
+				Preload("InstrumentNumber.IssuerNumber").
+				Preload("InvestingCompany").
+				Preload("InvestingCompany.Country").
+				First(&instrumentDateByCompany, p.Args["id"]).
+				Error; err != nil {
+				panic(err)
+			}
+
+			return instrumentDateByCompany, nil
+		},
+		Description: "Getting data of a single instrument date by company entry",
+	}
+}
---
+++ application/graphql/mutation/mutation.go
@@ -0,0 +1,36 @@
+package mutation
+
+import (
+	"fmt"
+	"github.com/graphql-go/graphql"
+	"github.com/jinzhu/gorm"
+)
+
+func Mutation(db *gorm.DB) *graphql.Object {
+	return graphql.NewObject(graphql.ObjectConfig{
+		Name: "Mutation",
+		Fields: graphql.Fields{
+			"create": &graphql.Field{
+				Type:        Results(),
+				Description: "Process a JSON data into the DB",
+				Args: graphql.FieldConfigArgument{
+					"input": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+				},
+				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+					payload := params.Args["input"].(string)
+
+					parsedPayload, err := JsonStringParse(payload)
+
+					if err != nil {
+						result := Result{Data: "Failed", Error: fmt.Sprintf("%s", err)}
+						return result, nil
+					}
+
+					return MigrateProcessedObject(parsedPayload, db), nil
+				},
+			},
+		},
+	})
+}
