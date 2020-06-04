83ba515238e79b64b9a76fb5ec56ad88dbda36ca
RoySegall <roy@segall.io>
2020-06-04 12:20:16 +0300

    Adding stuff

--- blog/src/components/Strips/Third/Strip.js
+++ blog/src/components/Strips/Third/Strip.js
@@ -1,7 +1,46 @@
 import React from "react"
 import "./style.scss";
+import {graphql, StaticQuery} from "gatsby";
 
-export const ThirdStrip = () => <section className="articles">
+const query = graphql`{
+    drupal {
+      nodeQuery(filter: {conditions: {field: "type", value: "article"}}) {
+        entities {
+          ... on drupal_NodeArticle {
+            title
+            fieldImage {
+              url
+              alt
+            }
+            fieldLink {
+              uri
+            }
+            fieldPublishingDate {
+              value
+            }
+            queryFieldAuthors {
+              entities {
+                entityId
+                entityLabel
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const article = (data) => {
+  return <div>
+    <h3 className="title"><a href="saads" target="_blank">{data.title}</a></h3>
+    <p className="sub-title">asasd, asasd</p>
+
+    <img src="{{$article_image.Permalink}}" alt="דוח ראשון" className="bordered"/>
+  </div>
+}
+
+export const articles = (data) => <section className="articles">
   <a id="articles"></a>
   <div className="text">
     <p className="medium">כתבות</p>
@@ -12,13 +51,10 @@ export const ThirdStrip = () => <section className="articles">
 
     <div className="grid-display">
 
-      <div>
-        <h3 className="title"><a href="saads" target="_blank">sadsad</a></h3>
-        <p className="sub-title">asasd, asasd</p>
-
-        <img src="{{$article_image.Permalink}}" alt="דוח ראשון" className="bordered"/>
-      </div>
+      {data.drupal.nodeQuery.entities.map(item => article(item))}
     </div>
   </div>
 
 </section>
+
+export const ThirdStrip = () => <StaticQuery query={query} render={data => {return articles(data)}}></StaticQuery>
