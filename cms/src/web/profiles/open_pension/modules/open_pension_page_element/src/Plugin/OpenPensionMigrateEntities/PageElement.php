7a961659d4af186910dccf6a60040b44fd245eb0
RoySegall <roy@segall.io>
2020-06-09 07:34:38 +0300

    Done with the nodes migrations

--- cms/src/web/profiles/open_pension/modules/open_pension_page_element/src/Plugin/OpenPensionMigrateEntities/PageElement.php
+++ cms/src/web/profiles/open_pension/modules/open_pension_page_element/src/Plugin/OpenPensionMigrateEntities/PageElement.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\open_pension_blog\Plugin\OpenPensionMigrateEntities;
+namespace Drupal\open_pension_page_element\Plugin\OpenPensionMigrateEntities;
 
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\open_pension_migrate\OpenPensionMigrateEntitiesPluginBase;
@@ -9,33 +9,31 @@ use Drupal\open_pension_migrate\OpenPensionMigrateEntitiesPluginBase;
  * Plugin implementation of the open_pension_migrate_entities.
  *
  * @OpenPensionMigrateEntities(
- *   id = "blog",
- *   label = @Translation("Blog"),
+ *   id = "page_element",
+ *   label = @Translation("Page element"),
  *   entity = "node"
  * )
  */
-class Blog extends OpenPensionMigrateEntitiesPluginBase {
+class PageElement extends OpenPensionMigrateEntitiesPluginBase {
 
   protected function getRows() {
     return [
-      ['title' => 'החזקות', 'file' => 'holdings'],
-      ['title' => 'אלוקיות', 'file' => 'allocations'],
-      ['title' => 'ביצועים', 'file' => 'performance'],
+      ['title' => 'עמוד ראשי - אודותינו', 'file' => 'front-page-about-us.html'],
+      ['title' => 'עמוד ראשי - כסף', 'file' => 'front-page-money.html'],
     ];
   }
 
   protected function processRow(EntityStorageInterface $entity, array $row_data) {
     $values = [
-      'type' => 'blog',
+      'type' => 'page_element',
       'title' => $row_data['title'],
       'langcode' => 'en',
       'body' => [
-        'value' => file_get_contents($this->getAssetLibrary('open_pension_blog', "{$row_data['file']}.html")),
+        'value' => file_get_contents($this->getAssetLibrary('open_pension_page_element', $row_data['file'])),
         'format' => 'full_html',
         'summary' => '',
         ],
       'uid' => 1,
-      'field_image' => $this->createFileObject('open_pension_blog', "{$row_data['file']}.png")
     ];
 
     return $entity->create($values);
