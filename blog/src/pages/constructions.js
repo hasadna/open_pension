b515666f68eae29d1ef10e13128431be187a0cad
RoySegall <roy@segall.io>
2020-09-21 08:46:45 +0300

    Adding the construction page

--- blog/src/pages/constructions.js
+++ blog/src/pages/constructions.js
@@ -1,6 +1,9 @@
 import React from "react";
 import {Wrapper} from "../components/Page";
 import {Helmet} from "react-helmet";
+import "./constructions.scss";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faHammer} from "@fortawesome/free-solid-svg-icons";
 
 export default function constructions() {
     return (
@@ -9,9 +12,22 @@ export default function constructions() {
                 <meta charSet="utf-8"/>
                 <title>פנסיה פתוחה | עבודים על זה</title>
             </Helmet>
-            <h1>עובדים על זה</h1>
 
-            <p>היי, אנחנו עובדים על החלק הזה עדיין.</p>
+            <div className="constructions">
+                <article>
+                    <section className="text">
+                        <h2>האיזור האישי בתהליכי פיתוח</h2>
+                        <p>היי, החלק הזה בתהליכי פיתוח ואנחנו מקווים לסיים את זה מהר.</p>
+                        <p>באיזור האישי תוכלו לראות היכן הכסף ששמור בקרן הפנסיה שלכם מושקע: נכסי נדל״ו, מט״ח, אג״חים ועוד.</p>
+                    </section>
+
+                    <section className="icon">
+                        <FontAwesomeIcon icon={faHammer} />
+                    </section>
+                </article>
+
+            </div>
+
         </Wrapper>
     )
 }
