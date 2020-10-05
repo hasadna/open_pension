f0f87d292f0a7bf72de16386be60d687208bfaa6
RoySegall <roy@segall.io>
2020-10-05 20:40:08 +0300

    Migrate the staff to the drupal side

--- blog/src/components/Staff/Staff.js
+++ blog/src/components/Staff/Staff.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React from "react"
 import "./Staff.scss"
-import Roy from "./roy.jpeg"
+import {StaticQuery, graphql} from "gatsby"
 
-export default () => {
-    const staff = [
-        {name: 'רועי סגל', position: 'מוביל טכני', picture: Roy}
-    ];
-    return <div className="staff">
-        {staff.map((peron, key) => <div key={key} className="person">
-            <img src={peron.picture} alt={`תמונה של ${peron.name}`}/>
-
-            <div className="info">
-                <span className="name">{peron.name}</span>
-                <span className="position">{peron.position}</span>
-            </div>
-        </div>)}
-
-
-    </div>
+const query = graphql`{
+  drupal {
+    taxonomyTermQuery(
+      filter: {conditions: [
+        {field: "vid", value: ["staff"]}, 
+        {field: "status", value: "1"}
+      ]}, 
+      sort: {field: "weight"}
+    ) {
+      entities {
+        ... on drupal_TaxonomyTermStaff {
+          entityLabel
+          fieldPosition
+          fieldProfilePicture {
+            url
+          }
+        }
+      }
+    }
+  }
 }
+`;
+
+const Personal = ({personal}) => <div className="person">
+  <img src={personal.fieldProfilePicture.url} alt={`תמונה של ${personal.entityLabel}`}/>
+     <div className="info">
+         <span className="name">{personal.entityLabel}</span>
+         <span className="position">{personal.fieldPosition}</span>
+     </div>
+  </div>
+
+export default () => <StaticQuery
+  query={query}
+  render={data =>
+    <div className="staff">
+      {data.drupal.taxonomyTermQuery.entities.map((personal, key) => <Personal key={key} personal={personal} />)}
+    </div>
+  }
+/>
