18bd0cbe3d0708b3df489b307cb87ff6929fd8c8
RoySegall <roy@segall.io>
2020-10-21 20:31:53 +0300

    Done with all the todos

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
         // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
         fieldName: "drupal",
         // Url to query from
-        url: `http://localhost/open_pension/cms/src/web/graphql`,
+        url: `http://cms/graphql`,
       },
     },
     {
