777d73bbd614dd391ccbdc127f790edf7ad9f2df
RoySegall <roy@segall.io>
2020-11-09 22:50:45 +0200

    Adding support for the events of kafka after updating.

---
+++ cms/src/web/profiles/open_pension/modules/open_pension_kafka/src/Plugin/KafkaTopic/processingStarted.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Drupal\open_pension_kafka\Plugin\KafkaTopic;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Logger\LoggerChannel;
+use Drupal\open_pension_files\Entity\OpenPensionStorageFiles;
+use Drupal\open_pension_files\OpenPensionFiles;
+use Drupal\open_pension_kafka\KafkaTopicPluginBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Plugin implementation of the kafka_topic.
+ *
+ * @KafkaTopic(
+ *   id = "processingStarted",
+ *   label = @Translation("File downloaded"),
+ *   description = @Translation("Handling when file was downloaded")
+ * )
+ */
+class processingStarted extends KafkaTopicPluginBase {
+
+  /**
+   * @var EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Open pension logger.
+   *
+   * @var LoggerChannel
+   */
+  protected $logger;
+
+  /**
+   * FileStored constructor.
+   *
+   * @param array $configuration
+   * @param $plugin_id
+   * @param $plugin_definition
+   * @param EntityTypeManagerInterface $entity_type_manager
+   * @param LoggerChannel $open_pension_logger
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, LoggerChannel $open_pension_logger) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->entityTypeManager = $entity_type_manager;
+    $this->logger = $open_pension_logger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('logger.open_pension_kafka')
+    );
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function handleTopicMessage($payload) {
+    $payload = json_decode($payload);
+
+    // todo: check here the file entry by the storage ID which changed and set
+    //  the status of the file.
+  }
+}
