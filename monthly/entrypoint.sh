d49eb1f3f35121b1911ba14444b621dfa4cc6826
RoySegall <roy@segall.io>
2021-01-18 10:14:35 +0200

    Dockering the app

---
+++ monthly/entrypoint.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Check if DB exists.DB
+echo "Waiting for DB connection"
+# todo: move to deticated env
+while ! echo exit | nc -vz monthly_db 3306;
+do
+    echo "DB still off-line....";
+    sleep 10;
+done
+echo "DB is online"
+
+# Migrating the DB.
+
+npm run clientGenerate
+npm run migrate
+
+npm run start
