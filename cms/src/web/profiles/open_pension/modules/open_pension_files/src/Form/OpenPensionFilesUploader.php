92a3640bfd5443935eb2669180b712ce4c30f25d
RoySegall <roy@segall.io>
2021-02-20 13:59:47 +0200

    Handle the storage file when uploading a file

--- cms/src/web/profiles/open_pension/modules/open_pension_files/src/Form/OpenPensionFilesUploader.php
+++ cms/src/web/profiles/open_pension/modules/open_pension_files/src/Form/OpenPensionFilesUploader.php
@@ -8,8 +8,8 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\Url;
 use Drupal\file\Entity\File;
-use Drupal\media\Entity\Media;
 use Drupal\open_pension_files\OpenPensionFilesProcessInterface;
+use Drupal\open_pension_services\OpenPensionServicesHealthStatus;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -31,10 +31,16 @@ class OpenPensionFilesUploader extends FormBase {
   protected $fileProcessorService;
 
   /**
+   * @var OpenPensionServicesHealthStatus
+   */
+  protected $healthServiceStatus;
+
+  /**
    * OpenPensionFilesUploader constructor.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_manager
    *   The entity service.
+   * @param OpenPensionServicesHealthStatus $health_service_health_status
    *
    * @param MessengerInterface $messenger
    * @param OpenPensionFilesProcessInterface $file_processor
@@ -44,11 +50,13 @@ class OpenPensionFilesUploader extends FormBase {
   public function __construct(
     EntityTypeManagerInterface $entity_manager,
     MessengerInterface $messenger,
-    OpenPensionFilesProcessInterface $file_processor
+    OpenPensionFilesProcessInterface $file_processor,
+    OpenPensionServicesHealthStatus $health_service_health_status
   ) {
     $this->fileStorage = $entity_manager->getStorage('file');
     $this->messenger = $messenger;
     $this->fileProcessorService = $file_processor;
+    $this->healthServiceStatus = $health_service_health_status;
   }
   // @codingStandardsIgnoreEnd
 
@@ -59,7 +67,8 @@ class OpenPensionFilesUploader extends FormBase {
     return new static(
       $container->get('entity_type.manager'),
       $container->get('messenger'),
-      $container->get('open_pension_files.file_process')
+      $container->get('open_pension_files.file_process'),
+      $container->get('open_pension_services.health_status')
     );
   }
 
@@ -74,7 +83,12 @@ class OpenPensionFilesUploader extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    // todo: check here the status of the storage service.
+    // Checking the health of the service.
+    $storageServiceDown = $this->healthServiceStatus->getStorageState() === OpenPensionServicesHealthStatus::SERVICE_NOT_RESPONDING;
+    if ($storageServiceDown) {
+      $this->messenger->addError(t('The storage service is not responding. Please check it\'s on the air'), 'error');
+    }
+
     $form['selected_files'] = [
       '#title' => t('Select files to upload'),
       '#description' => t('You can add as many files as you want. The files will be sent to processor for parsing and the rest of the flow.'),
@@ -86,6 +100,7 @@ class OpenPensionFilesUploader extends FormBase {
 
     $form['actions'] = [
       '#type' => 'actions',
+      '#disabled' => $storageServiceDown,
       'submit' => [
         '#type' => 'submit',
         '#value' => t('Submit'),
