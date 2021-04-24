c75c9560a80a88905f65e36958cb82b232c861c1
RoySegall <roy@segall.io>
2021-04-24 23:07:36 +0300

    Minor touch ups

---
+++ front/Components/WelcomeMessage/WelcomeMessage.js
@@ -0,0 +1,15 @@
+export default function WelcomeMessage() {
+  return <div className="welcome-message">
+
+    <h2>
+      פנסיה פתוחה - מדובר על הכסף שלך
+    </h2>
+
+    <p>
+      מהיום תוכל לדעת בצורה פשוטה מה קורה עם הכסף שלך,
+      היכן הוא מושקע וכמה הוא מרוויח.
+    </p>
+
+    <a>מה קורה עם הכסף שלי?</a>
+  </div>
+}
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,7 +1,8 @@
 import Wrapper from "../Components/Wrapper/wrapper";
+import WelcomeMessage from "../Components/WelcomeMessage/WelcomeMessage";
 
 export default function Home() {
   return <Wrapper title="Homepage">
-    Home
+    <WelcomeMessage />
   </Wrapper>
 }
