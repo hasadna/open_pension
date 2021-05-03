1755ac9663f206be316a7163f2a626eedf0a5587
RoySegall <roy@segall.io>
2021-05-03 08:23:08 +0300

    Working on the graph design

--- front/Components/PerformanceResults/PerformanceResults.js
+++ front/Components/PerformanceResults/PerformanceResults.js
@@ -1,5 +1,6 @@
 import Table from "../Table/Table";
 import {useState} from 'react';
+import { Line } from 'react-chartjs-2';
 
 export default function PerformanceResults({tracksInfo}) {
   const [selectedFilter, setSelectedFilter] = useState('last12Years');
@@ -11,6 +12,38 @@ export default function PerformanceResults({tracksInfo}) {
     last12Years: '12 חודשים אחרונים',
     last3Years: '3 שנים אחרונות',
     last5Years: '5 שנים אחרונות',
+  };
+
+  const data = {
+    labels: ['1', '2', '3', '4', '5', '6'],
+    datasets: [
+      {
+        label: 'My First Dataset',
+        data: [120, 200, 66, 81, 56, 200, 170],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      },
+      {
+        label: 'My SECPOND Dataset',
+        data: [120, 100, 90, 50, 56, 190, 150],
+        fill: false,
+        borderColor: 'rgb(188,192,75)',
+        tension: 0.1
+      },
+    ]
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
   };
 
   return <div className="performance-results">
@@ -43,6 +76,8 @@ export default function PerformanceResults({tracksInfo}) {
           </li>
         })}
       </ul>
+
+      <Line data={data} options={options} />
     </div>
   </div>
 }
