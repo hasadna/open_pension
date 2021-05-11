bd36ad91eed0d37f0fe349ee4eec8b19b9b511bf
RoySegall <roy@segall.io>
2021-05-11 07:46:17 +0300

    Adding the underconstruction body

--- front/pages/portfolio.js
+++ front/pages/portfolio.js
@@ -1,9 +1,20 @@
 import Wrapper from "../Components/Wrapper/Wrapper";
+import SecondaryHeader from "../Components/SecondaryHeader/SecondaryHeader";
 
 export default function Portfolio() {
   return <>
     <Wrapper title="מבנה תיק">
-      תחת בניה
+      <SecondaryHeader
+        title={"מבנה תיק"}
+        description={<p className="under-construction"> אנחנו עובדים על סיום העמוד, עוד קצת סבלנות.</p>}
+      >
+      </SecondaryHeader>
+
+      <div className="inner-page-content">
+        <div className="under-construction-image">
+          <img src="./svgs/under-constructions.svg" />
+        </div>
+      </div>
     </Wrapper>
   </>
 }
