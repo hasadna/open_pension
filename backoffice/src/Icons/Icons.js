7ecb77184614289f8fd55361b0d6de8ae9b8c4d2
Roy Segall <roy@segall.io>
2021-05-27 09:07:36 +0300

    #599: Adding sub menu.

--- backoffice/src/Icons/Icons.js
+++ backoffice/src/Icons/Icons.js
@@ -1,8 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faFile, faDatabase, faClock, faTasks, faChevronCircleDown, faChevronLeft, faChevronRight, faHome,
-  faUsers, faCopy, faChartBar, faUpload, faTrash, faPencilAlt, faSearch
+  faUsers, faCopy, faChartBar, faUpload, faTrash, faPencilAlt, faSearch, faBook, faBookOpen, faParagraph,
+  faInfoCircle, faShekelSign, faRoute
 } from '@fortawesome/free-solid-svg-icons';
+import {faYoutube} from "@fortawesome/free-brands-svg-icons";
 
 export const File = () => <FontAwesomeIcon icon={faFile} />
 export const Database = () => <FontAwesomeIcon icon={faDatabase} />
@@ -19,3 +21,10 @@ export const Charts = () => <FontAwesomeIcon icon={faChartBar} />
 export const Upload = () => <FontAwesomeIcon icon={faUpload} />
 export const Edit = () => <FontAwesomeIcon icon={faPencilAlt} />
 export const Delete = () => <FontAwesomeIcon icon={faTrash} />
+export const Book = () => <FontAwesomeIcon icon={faBook} />
+export const BookOpen = () => <FontAwesomeIcon icon={faBookOpen} />
+export const Paragraph = () => <FontAwesomeIcon icon={faParagraph} />
+export const InfoCircle = () => <FontAwesomeIcon icon={faInfoCircle} />
+export const Shekel = () => <FontAwesomeIcon icon={faShekelSign} />
+export const Route = () => <FontAwesomeIcon icon={faRoute} />
+export const Articles = () => <FontAwesomeIcon icon={faYoutube} />
