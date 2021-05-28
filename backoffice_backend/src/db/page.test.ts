6de126185240e4cc2b5a00b96a6a41764b483363
RoySegall <roy@segall.io>
2021-05-28 13:49:30 +0300

    Adding base crud actions

--- backoffice_backend/src/db/page.test.ts
+++ backoffice_backend/src/db/page.test.ts
@@ -1,18 +1,52 @@
+import {
+  createPage,
+  deletePage,
+  getPage,
+  PageInterface,
+  updatePage
+} from "./page";
+
 describe('Page', () => {
+
+  const dummyPage: PageInterface = {label: 'first page'};
+
   it('Creating a page with unique label', async() => {
-    expect(1).toBe(1);
+    const {errors, object} = await createPage(dummyPage);
+    expect(errors).toBeNull();
+    expect(object.label).toBe('first page');
   });
 
-  it('Should failed when creating a page with the same label', async() => {
-    expect(1).toBe(1);
+  it('Should failed when not passing a label', async() => {
+    await createPage(dummyPage);
+
+    // @ts-ignore
+    const {errors, object} = await createPage({food: 'pizza'});
+
+    expect(object).toBeNull();
+    expect(errors).toStrictEqual({ label: 'Path `label` is required.' })
   });
 
   it('Testing an update of a page', async() => {
-    expect(1).toBe(1);
+    const {object: {_id: id}} = await createPage(dummyPage);
+    const {label} = await updatePage(id, {label: 'second page'});
+    expect(label).toBe('second page');
+
+    // Reloading the page from the DB.
+    const {collections: {_id: reloadedId, label: reloadedLabel}} = await getPage({id});
+
+    expect(String(reloadedId)).toBe(String(id));
+    expect(reloadedLabel).toBe('second page');
   });
 
   it('Deleting a page', async() => {
-    expect(1).toBe(1);
+    const {object: {_id: id}} = await createPage(dummyPage);
+    const {collections} = await getPage({id});
+    expect(collections).not.toBeNull();
+
+    // Deleting the page and verify we cannot load it again.
+    await deletePage(id);
+    const {collections: collectionAfterDelete} = await getPage({id});
+    expect(collectionAfterDelete).toBeNull();
   });
 
   it('Server: Creating a page', async () => {
