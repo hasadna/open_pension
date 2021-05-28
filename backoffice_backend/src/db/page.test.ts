806dce929cdbf5613c20189db8c46b24ef1f31a2
RoySegall <roy@segall.io>
2021-05-28 10:32:20 +0300

    Adding the base crud function for a page and adding an STP

---
+++ backoffice_backend/src/db/page.test.ts
@@ -0,0 +1,37 @@
+describe('Page', () => {
+  it('Creating a page with unique label', async() => {
+    expect(1).toBe(1);
+  });
+
+  it('Should failed when creating a page with the same label', async() => {
+    expect(1).toBe(1);
+  });
+
+  it('Testing an update of a page', async() => {
+    expect(1).toBe(1);
+  });
+
+  it('Deleting a page', async() => {
+    expect(1).toBe(1);
+  });
+
+  it('Server: Creating a page', async () => {
+    expect(1).toBe(1);
+  });
+
+  it('Server: Updating and creating a page with a non unique label', async () => {
+    expect(1).toBe(1);
+  });
+
+  it('Server: Updating a page', async () => {
+    expect(1).toBe(1);
+  });
+
+  it('Server: Delete a page', async () => {
+    expect(1).toBe(1);
+  });
+
+  it('Server: Get a page specific page', async () => {
+    expect(1).toBe(1);
+  });
+});
