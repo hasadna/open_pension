35f65274d199acc454141e7d5faab7ab92a46ac7
RoySegall <roy@segall.io>
2021-06-07 07:24:41 +0300

    Adding the new models in the DB and add the resolvers

---
+++ monthly/prisma/migrations/20210607034915_adding_insurance_type_body_pension_route/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE `InsuranceType` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `InvestmentType` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Body` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
