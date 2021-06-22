b576e9ebe28bdee2b425093f757a6579a4ebc81c
RoySegall <roy@segall.io>
2021-06-22 07:21:44 +0300

    Remove more stuff

--- monthly/src/server/resolvers.ts
+++ monthly/src/server/resolvers.ts
@@ -1,25 +1,18 @@
 export default {
   Query: {
     channels: async (_, __, ctx) => {
-      // @ts-ignore
       const {prisma} = ctx;
       return await prisma.channel.findMany({});
     },
     fundNames: async (_, __, ctx) => {
-      // @ts-ignore
-
       const {prisma} = ctx;
       return await prisma.fundName.findMany({});
     },
     managingBodies: async (_, __, ctx) => {
-      // @ts-ignore
-
       const {prisma} = ctx;
       return await prisma.managingBody.findMany({});
     },
     subChannels: async (_, __, ctx) => {
-      // @ts-ignore
-
       const {prisma} = ctx;
       return await prisma.subChannel.findMany({});
     },
