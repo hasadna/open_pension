f0256acd3950918f5bb4ac05fdd1110543f3a642
RoySegall <roy@segall.io>
2021-07-12 06:58:00 +0300

    Adding the resolver

--- monthly/src/server/resolvers.ts
+++ monthly/src/server/resolvers.ts
@@ -16,5 +16,21 @@ export default {
       const {prisma} = ctx;
       return await prisma.subChannel.findMany({});
     },
+
+    lastUpdated: async (_, __, ctx) => {
+      const {prisma} = ctx;
+
+      const {TKUFAT_DIVUACH} = await prisma.row.findFirst({
+        take: 1,
+        orderBy: {
+          TKUFAT_DIVUACH: 'desc'
+        },
+        select: {
+          TKUFAT_DIVUACH: true
+        },
+      });
+
+      return TKUFAT_DIVUACH.getTime() / 1000;
+    },
   },
 };
