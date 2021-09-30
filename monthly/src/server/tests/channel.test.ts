cbfe0af0ac03538c595352dc44251273ebbde08b
RoySegall <roy@segall.io>
2021-09-30 10:06:53 +0300

    Adding the test for the channels

--- monthly/src/server/tests/channel.test.ts
+++ monthly/src/server/tests/channel.test.ts
@@ -1,4 +1,4 @@
-// import {prisma} from '../context';
+import {prisma} from '../context';
 import {createTestingServer, sendQuery} from "./testingServer";
 import {channelsQuery} from "./testingQueries";
 
@@ -15,11 +15,32 @@ describe('Channel', () => {
     expect(channels).toStrictEqual([]);
   });
 
-  it('Send query when there is a single channel', () => {
-    expect(1).toBe(1);
+  it('Send query when there is a single channel', async () => {
+    const {ID, label} = await prisma.channel.create({
+      data: {
+        label: 'Label',
+        prefix: 'l'
+      }
+    });
+
+    const {data: {channels: [channel]}} = await sendQuery(channelsQuery, testingServer);
+    expect(channel.ID).toBe(ID);
+    expect(channel.label).toBe(label);
   });
 
-  it('Send query when there are two channels', () => {
-    expect(1).toBe(1);
+  it('Send query when there are two channels', async () => {
+    await prisma.channel.createMany({data: [
+        {label: 'First label', prefix: 'fl'},
+        {label: 'Second label', prefix: 'sl'}
+      ]}
+    );
+
+    const {data: {channels: [firstChannel, secondChannel]}} = await sendQuery(channelsQuery, testingServer);
+
+    expect(firstChannel.ID).not.toBeNull();
+    expect(firstChannel.label).toBe('First label');
+
+    expect(secondChannel.ID).not.toBeNull();
+    expect(secondChannel.label).toBe('Second label');
   });
 });
