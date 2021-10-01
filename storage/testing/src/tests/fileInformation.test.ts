8fdcf0ef09970689a832fe696cda2d83d360ae05
RoySegall <roy@segall.io>
2021-10-01 23:20:35 +0300

    Adding the cat image for testing and complete the test

--- storage/testing/src/tests/fileInformation.test.ts
+++ storage/testing/src/tests/fileInformation.test.ts
@@ -1,5 +1,8 @@
 import {getAxios} from "./utils";
 import {AxiosError} from "./interfaces";
+import {prisma} from "../server/context";
+import {join} from 'path';
+import {copyFileSync} from "fs";
 
 describe('File information', () => {
 
@@ -14,11 +17,31 @@ describe('File information', () => {
   });
 
   it('Requesting a file: png', async () => {
-    expect(1).toBe(1);
+    // Copy the file from the assets to the volume of the files which belong to the storage.
+    copyFileSync(
+      join(process.cwd(), 'src', 'tests', 'assets', 'cat.png'),
+      join(process.cwd(), '../', '../', 'data', 'files', 'storage', 'cat.png')
+    );
+    const file = await prisma.file.create({
+      data: {
+        filename: 'cat.png',
+        path: '/home/app/download_files/cat.png',
+      },
+    });
+
+    const {headers, data} = await getAxios().get(`/file/${file.id}`);
+    expect(headers['content-type']).toBe('image/png');
+
+    // @ts-ignore
+    expect(data.includes('PNG')).toBeTruthy();
   });
 
   it('Requesting a file: xml', async () => {
     expect(1).toBe(1);
+  });
+
+  it('Requesting a file with a path which does not exists', async () => {
+
   });
 
   it('Getting all the file from the graphql endpoint', async () => {
