8c372aba8182e9f742ff158dd3efb54a0f74f568
RoySegall <roy@segall.io>
2021-10-01 12:20:30 +0300

    Adding the storage testing project

---
+++ storage/testing/src/tests/fileCreation.test.ts
@@ -0,0 +1,11 @@
+describe('File uploading', () => {
+
+  it('Uploading file', async () => {
+    expect(1).toBe(1);
+  });
+
+  it('Uploading file which already been uploaded', async () => {
+    // todo: https://github.com/hasadna/open_pension/issues/697
+    expect(1).toBe(1);
+  });
+});
---
+++ storage/testing/src/tests/fileInformation.test.ts
@@ -0,0 +1,18 @@
+describe('File information', () => {
+
+  it('Downloading a file which not exists', async () => {
+    expect(1).toBe(1);
+  });
+
+  it('Requesting a file: png', async () => {
+    expect(1).toBe(1);
+  });
+
+  it('Requesting a file: xml', async () => {
+    expect(1).toBe(1);
+  });
+
+  it('Getting all the file from the graphql endpoint', async () => {
+    expect(1).toBe(1);
+  });
+});
