5932f4610f4a5296c0f99a6ce1f5db9e0a60d592
RoySegall <roy@segall.io>
2021-10-02 13:30:27 +0300

    Working on the second test

--- storage/testing/src/tests/fileCreation.test.ts
+++ storage/testing/src/tests/fileCreation.test.ts
@@ -1,6 +1,21 @@
+import {uploadFile} from "./utils";
+import {catImageOriginPath} from "./constAndInterfaces";
+
 describe('File uploading', () => {
 
-  it('Uploading file', async () => {
+  it('Uploading file a single file', async () => {
+    const {data: {files: [file]}} = await uploadFile([catImageOriginPath]);
+    expect(file.ID).not.toBeNull();
+    expect(file.filename).not.toBe('cat.png');
+
+    // todo: get the file info from the graphql.
+  });
+
+  it('Uploading multiple files', async () => {
+    expect(1).toBe(1);
+  });
+
+  it('Uploading a zip file', async () => {
     expect(1).toBe(1);
   });
 
--- storage/testing/src/tests/utils.ts
+++ storage/testing/src/tests/utils.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import {prisma} from "../server/context";
 import {FileCreateType, FileResponse, FilesResponse} from "./constAndInterfaces";
+import {createReadStream} from "fs";
+const FormData = require('form-data'); // npm install --save form-data
 
 export function getAxios() {
   return axios.create({
@@ -9,6 +11,20 @@ export function getAxios() {
 }
 
 export const fileCreate = async (data: FileCreateType) => await prisma.file.create({data,});
+export const sendGraphQLQuery = async (query: string): Promise<FileResponse|FilesResponse> => getAxios().post(`/graphql`, {query});
+
+export const uploadFile = async (paths: string[]) => {
+  const form = new FormData();
+  paths.forEach(path => form.append('file', createReadStream(path)));
+
+  const request_config = {
+    headers: {
+      ...form.getHeaders()
+    }
+  };
+
+  return getAxios().post(`/file`, form, request_config);
+}
 
 export const createMultipleFiles = async () => {
   const filesData = [
@@ -20,6 +36,4 @@ export const createMultipleFiles = async () => {
 
   return Promise.all(filesData.map(data => fileCreate(data)));
 }
-
-export const sendGraphQLQuery = async (query: string): Promise<FileResponse|FilesResponse> => getAxios().post(`/graphql`, {query});
 
