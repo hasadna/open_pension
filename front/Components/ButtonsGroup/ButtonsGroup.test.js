08add9412e74ade9fac9325c4287676fbb6fc8f4
RoySegall <roy@segall.io>
2021-10-05 19:44:11 +0300

    Done with the last test

--- front/Components/ButtonsGroup/ButtonsGroup.test.js
+++ front/Components/ButtonsGroup/ButtonsGroup.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen, fireEvent } from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import './ButtonsGroup'
 import ButtonGroups from "./ButtonsGroup";
@@ -15,6 +15,12 @@ describe('Buttons group', () => {
     selectHandler={selectHandler}
     {...props}
   />;
+
+  const clickOnLink = (link, calledWith) => {
+    // Click on the first element.
+    fireEvent.click(link);
+    expect(selectHandler).toBeCalledWith(calledWith);
+  }
 
   it('Snapshot', () => {
     const trees = {
@@ -50,27 +56,46 @@ describe('Buttons group', () => {
     expect(firstLink.text).toBe('First option');
     expect(secondLink.text).toBe('Second option');
 
-    // Click on the first element.
-    fireEvent.click(firstLink);
-    expect(selectHandler).toBeCalledWith({"0": true});
+    clickOnLink(firstLink, {"0": true});
 
     // Verifying classes.
     expect(firstLink).toHaveClass('active');
     expect(secondLink).not.toHaveClass('active');
 
-    // Click on the second element.
-    fireEvent.click(secondLink);
-    expect(selectHandler).toBeCalledWith({"1": true});
+    clickOnLink(secondLink, {"1": true})
 
     // Verifying classes.
-    expect(firstLink).not.toHaveClass('active');
     expect(secondLink).toHaveClass('active');
+    expect(firstLink).not.toHaveClass('active');
 
     // Testing the total number invocations.
     expect(selectHandler).toBeCalledTimes(2);
   });
 
   it('Testing selection of multiple elements', () => {
-    render(getComponent());
+    render(getComponent({multiple: true}));
+    expect(selectHandler).not.toBeCalled();
+
+    const [firstLink, secondLink] = screen.getAllByTestId('item');
+
+    expect(firstLink.text).toBe('First option');
+    expect(secondLink.text).toBe('Second option');
+
+    // Turn on both of the items.
+    clickOnLink(firstLink, {"0": true});
+    expect(firstLink).toHaveClass('active');
+    expect(secondLink).not.toHaveClass('active');
+
+    clickOnLink(secondLink, {"0": true, "1": true});
+    expect(secondLink).toHaveClass('active');
+    expect(firstLink).toHaveClass('active');
+
+    // Click on the first item again and verify the classes.
+    clickOnLink(firstLink, {"0": true});
+    expect(firstLink).not.toHaveClass('active');
+    expect(secondLink).toHaveClass('active');
+
+    // Testing the total number invocations.
+    expect(selectHandler).toBeCalledTimes(3);
   });
 });
--- front/jest.setup.js
+++ front/jest.setup.js
@@ -1,1 +1,5 @@
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect';
+
+beforeEach(() => {
+  jest.resetAllMocks();
+})
