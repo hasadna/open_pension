fa6ab7755536ceeb0e93ed0cb816aa346c093845
RoySegall <roy@segall.io>
2021-10-11 07:30:36 +0300

    Doing it TDD

--- monthly/src/lib/queries/peformance.test.ts
+++ monthly/src/lib/queries/peformance.test.ts
@@ -6,7 +6,7 @@ describe('Performance', function () {
 
   describe('convertTimePeriodToTimeRangeQuery', () => {
     const firstFileDate = new Date(`2017-01-01T00:00:00.000Z`);
-    const secondFileDate = new Date(`2020-01-01T00:00:00.000Z`);
+    const secondFileDate = new Date('2020-01-01T00:00:00.000Z');
 
     beforeAll(async () => {
       // Create two records to verify we we'll pull the last one and calculate
@@ -22,15 +22,40 @@ describe('Performance', function () {
     it('Get the time ranges for 3 months', async () => {
       const {timeStartRange, timeEndRange} = await convertTimePeriodToTimeRangeQuery(TimePeriod.THREE_MONTHS);
 
-      expect(timeStartRange).toBe('2021-10-01T00:00:00.000Z');
-      expect(timeEndRange).toBe('2021-10-01T00:00:00.000Z');
-
+      expect(timeEndRange).toBe('2020-01-01T00:00:00.000Z');
+      expect(timeStartRange).toBe('2019-10-01T00:00:00.000Z');
     });
-    it('Get the time ranges for 6 months', async () => {});
-    it('Get the time ranges for year start', async () => {});
-    it('Get the time ranges for 12 months', async () => {});
-    it('Get the time ranges for 3 years', async () => {});
-    it('Get the time ranges for 5 years', async () => {});
+
+    it('Get the time ranges for 6 months', async () => {
+      const {timeStartRange, timeEndRange} = await convertTimePeriodToTimeRangeQuery(TimePeriod.THREE_MONTHS);
+
+      expect(timeEndRange).toBe('2020-01-01T00:00:00.000Z');
+      expect(timeStartRange).toBe('2019-07-01T00:00:00.000Z');
+    });
+    it('Get the time ranges for year start', async () => {
+      const {timeStartRange, timeEndRange} = await convertTimePeriodToTimeRangeQuery(TimePeriod.THREE_MONTHS);
+
+      expect(timeEndRange).toBe('2020-01-01T00:00:00.000Z');
+      expect(timeStartRange).toBe('2020-01-01T00:00:00.000Z');
+    });
+    it('Get the time ranges for 12 months', async () => {
+      const {timeStartRange, timeEndRange} = await convertTimePeriodToTimeRangeQuery(TimePeriod.THREE_MONTHS);
+
+      expect(timeEndRange).toBe('2020-01-01T00:00:00.000Z');
+      expect(timeStartRange).toBe('2019-01-01T00:00:00.000Z');
+    });
+    it('Get the time ranges for 3 years', async () => {
+      const {timeStartRange, timeEndRange} = await convertTimePeriodToTimeRangeQuery(TimePeriod.THREE_MONTHS);
+
+      expect(timeEndRange).toBe('2020-01-01T00:00:00.000Z');
+      expect(timeStartRange).toBe('2017-01-01T00:00:00.000Z');
+    });
+    it('Get the time ranges for 5 years', async () => {
+      const {timeStartRange, timeEndRange} = await convertTimePeriodToTimeRangeQuery(TimePeriod.THREE_MONTHS);
+
+      expect(timeEndRange).toBe('2020-01-01T00:00:00.000Z');
+      expect(timeStartRange).toBe('2015-01-01T00:00:00.000Z');
+    });
   });
 
 });
