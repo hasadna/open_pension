ba1893f0f8437158b23db86c8c04b4bb6dc15772
RoySegall <roy@segall.io>
2021-12-20 20:54:40 +0200

    Adding progress

--- services-health-checker/e2e/run-tests.js
+++ services-health-checker/e2e/run-tests.js
@@ -1,40 +1,42 @@
 const ngrok = require('ngrok');
 const {writeFileSync} = require('fs');
 const {join} = require('path');
-const { spawn } = require("child_process");
+const {spawn} = require("child_process");
 
-(async function() {
+(async function () {
+  try {
+    const backend = await ngrok.connect({authtoken: process.env.NGROKTOKEN, addr: 1000});
+    const front = await ngrok.connect({authtoken: process.env.NGROKTOKEN, addr: 3000});
 
-  const [backend, front] = await Promise.all([
-    ngrok.connect({authtoken: process.env.NGROKTOKEN, addr: 1000}),
-    ngrok.connect({authtoken: process.env.NGROKTOKEN, addr: 3000})
-  ]);
 
-  const configFilePath = join(process.cwd(), 'e2e', 'config.js');
+    const configFilePath = join(process.cwd(), 'e2e', 'config.js');
 
-  writeFileSync(configFilePath,  `exports.config = { 
+    writeFileSync(configFilePath, `exports.config = { 
     beforeTest: function (test) {
       return ${JSON.stringify({backend, front})} 
     } 
   }`);
-  const ls = spawn(`testim`,  [
-    `-c`, './e2e/config.js',
-    "--token", process.env.TESTIM_TOKEN,
-    "--project", process.env.TESTIM_PROJECT,
-    "--base-url", "http://www.google.com",
-    "--grid", "Testim-Grid",
-    "--chrome-binary-location",  "/usr/bin/google-chrome"
-  ]);
+    const ls = spawn(`testim`, [
+      `-c`, './e2e/config.js',
+      "--token", process.env.TESTIM_TOKEN,
+      "--project", process.env.TESTIM_PROJECT,
+      "--base-url", "http://www.google.com",
+      "--grid", "Testim-Grid",
+      "--chrome-binary-location", "/usr/bin/google-chrome"
+    ]);
 
-  ls.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-  });
+    ls.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
 
-  ls.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
+    ls.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
 
-  ls.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
+    ls.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 })();
