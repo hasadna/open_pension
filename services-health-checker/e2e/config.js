991924038430a2f316369b88af60bc8cf2583f2c
RoySegall <roy@segall.io>
2021-12-20 20:14:08 +0200

    Adding progress

--- services-health-checker/e2e/config.js
+++
@@ -1,7 +0,0 @@
-exports.config = { 
-    beforeTest: function (test) {
-      console.log(test.config); 
-      test.config.baseUrl = 'https://5535-77-125-98-78.ngrok.io'; 
-      return {"backend":"https://5535-77-125-98-78.ngrok.io","front":"https://1480-77-125-98-78.ngrok.io"} 
-    } 
-  }
--- services-health-checker/e2e/run-tests.js
+++ services-health-checker/e2e/run-tests.js
@@ -6,15 +6,14 @@ const { spawn } = require("child_process");
 (async function() {
 
   const [backend, front] = await Promise.all([
-    ngrok.connect({authtoken: '22EemEoKm8N5d4qYLbHunNbUqHm_6Pko7dcuwP6Dzw3z59jqh', addr: 1000}),
-    ngrok.connect({authtoken: '22EemEoKm8N5d4qYLbHunNbUqHm_6Pko7dcuwP6Dzw3z59jqh', addr: 3000})
+    ngrok.connect({authtoken: process.env.ngrokToken, addr: 1000}),
+    ngrok.connect({authtoken: process.env.ngrokToken, addr: 3000})
   ]);
 
   const configFilePath = join(process.cwd(), 'e2e', 'config.js');
 
   writeFileSync(configFilePath,  `exports.config = { 
     beforeTest: function (test) {
-      test.config.baseUrl = '${backend}'; 
       return ${JSON.stringify({backend, front})} 
     } 
   }`);
@@ -22,6 +21,7 @@ const { spawn } = require("child_process");
     `-c`, './e2e/config.js',
     "--token", process.env.TESTIM_TOKEN,
     "--project", process.env.TESTIM_PROJECT,
+    "--base-url", "http://www.google.com",
     "--grid", "Testim-Grid",
     "--chrome-binary-location",  "/usr/bin/google-chrome"
   ]);
